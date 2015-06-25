0301b732c8eb35bbf80a0c3ca0a9685613b7579c
mrelac <mrelac@ebi.ac.uk>
2015-06-25 12:14:27 +0100

    Broke DownloadIMSRReport into a generic DownloadStream and DownloadIMSRReport.

--- loads/src/main/java/org/mousephenotype/cda/loads/DownloadIMSRReport.java
+++ loads/src/main/java/org/mousephenotype/cda/loads/DownloadIMSRReport.java
@@ -16,16 +16,11 @@
 
 package org.mousephenotype.cda.loads;
 
-import org.apache.http.HttpEntity;
-import org.apache.http.client.HttpClient;
-import org.apache.http.client.methods.CloseableHttpResponse;
-import org.apache.http.client.methods.HttpGet;
-import org.apache.http.impl.client.CloseableHttpClient;
-import org.apache.http.impl.client.HttpClients;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-import java.io.*;
+import java.io.File;
+import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
@@ -40,75 +35,92 @@ import java.nio.file.Paths;
 public class DownloadIMSRReport {
     private final Logger logger = LoggerFactory.getLogger(this.getClass());
     private final String IMSR_URL = "http://www.findmice.org/report.txt?query=&states=Any&_states=1&types=Any&_types=1&repositories=Any&_repositories=1&_mutations=on&results=500000&startIndex=0&sort=score&dir=";
-    private final String HTTP_PROXY = "http://hx-wwwcache.ebi.ac.uk:3128";
-    private HttpClient httpClient;
-    private Path fqDirectoryPath;
-    private Path fqFilePath;
-    private String workspace;
+    private final String TARGET = "./imsr/report.txt";
+//    private final String HTTP_PROXY = "http://hx-wwwcache.ebi.ac.uk:3128";
+//    private HttpClient httpClient;
+//    private Path fqDirectoryPath;
+//    private Path fqFilePath;
+//    private String workspace;
 
 
     public DownloadIMSRReport() throws IOException {
-        workspace = (System.getProperty("WORKSPACE") == null ? "." : System.getProperty("WORKSPACE"));
-        fqDirectoryPath = Paths.get(workspace + "/imsr");
-        fqFilePath = Paths.get(fqDirectoryPath.toAbsolutePath() + "/report.txt");
+
     }
 
-    public void downloadReport() throws IOException {
-        createDirectory();
-        final Process p;
-        try {
-            CloseableHttpClient httpClient = HttpClients.createDefault();
-            HttpGet httpGet = new HttpGet(IMSR_URL);
-            CloseableHttpResponse response = httpClient.execute(httpGet);
+    public File downloadReport() throws IOException {
+        boolean append = false;
+        DownloadStream downloadStream = new DownloadStream(IMSR_URL, TARGET, append);
+        return downloadStream.getReport();
 
-            HttpEntity entity = response.getEntity();
-            InputStream inputStream = entity.getContent();
-            BufferedReader input = new BufferedReader(new InputStreamReader(inputStream));
-            String line = null;
 
-            try {
-                File outputFile = new File(fqFilePath.toString());
-                boolean append = false;
-                FileWriter writer = new FileWriter(outputFile, append);
-                while ((line = input.readLine()) != null) {
-                    writer.write(line + "\n");
-                }
-                writer.close();
-            } catch (IOException e) {
-                e.printStackTrace();
-            }
 
-            Path outputFilePath = Paths.get(fqFilePath.toString());
-            logger.info("Wrote " + Files.size(outputFilePath) + " bytes to " + outputFilePath.toAbsolutePath());
-
-        } catch (Exception e) {
-            logger.error("Exception: " + e.getLocalizedMessage());
-            throw new IOException(e.getLocalizedMessage());
-        }
+//        workspace = (System.getProperty("WORKSPACE") == null ? "." : System.getProperty("WORKSPACE"));
+//        fqDirectoryPath = Paths.get(workspace + "/imsr");
+//        fqFilePath = Paths.get(fqDirectoryPath.toAbsolutePath() + "/report.txt");
     }
+
+//    public void downloadReport() throws IOException {
+//        createDirectory();
+//        final Process p;
+//        try {
+//            CloseableHttpClient httpClient = HttpClients.createDefault();
+//            HttpGet httpGet = new HttpGet(IMSR_URL);
+//            CloseableHttpResponse response = httpClient.execute(httpGet);
+//
+//            HttpEntity entity = response.getEntity();
+//            InputStream inputStream = entity.getContent();
+//            BufferedReader input = new BufferedReader(new InputStreamReader(inputStream));
+//            String line = null;
+//
+//            try {
+//                File outputFile = new File(fqFilePath.toString());
+//                boolean append = false;
+//                FileWriter writer = new FileWriter(outputFile, append);
+//                while ((line = input.readLine()) != null) {
+//                    writer.write(line + "\n");
+//                }
+//                writer.close();
+//            } catch (IOException e) {
+//                e.printStackTrace();
+//            }
+//
+//            Path outputFilePath = Paths.get(fqFilePath.toString());
+//            logger.info("Wrote " + Files.size(outputFilePath) + " bytes to " + outputFilePath.toAbsolutePath());
+//
+//        } catch (Exception e) {
+//            logger.error("Exception: " + e.getLocalizedMessage());
+//            throw new IOException(e.getLocalizedMessage());
+//        }
+//    }
 
 
     // PRIVATE METHODS
 
 
-    /**
-     * Create the IMSR directory if it doesn't already exist.
-     *
-     * @throws IOException
-     */
-    private void createDirectory() throws IOException {
-        if ( ! Files.isDirectory(fqDirectoryPath)) {
-            Files.createDirectory(fqDirectoryPath);
-        }
-        logger.info("Using IMSR load directory " + fqDirectoryPath.toAbsolutePath().toString());
-    }
+//    /**
+//     * Create the IMSR directory if it doesn't already exist.
+//     *
+//     * @throws IOException
+//     */
+//    private void createDirectory() throws IOException {
+//        if ( ! Files.isDirectory(fqDirectoryPath)) {
+//            Files.createDirectory(fqDirectoryPath);
+//        }
+//        logger.info("Using IMSR load directory " + fqDirectoryPath.toAbsolutePath().toString());
+//    }
 
 
     // MAIN
 
 
-    public static void main(String[] args) throws IOException {
-        DownloadIMSRReport downloadISMRReports = new DownloadIMSRReport();
-        downloadISMRReports.downloadReport();
+    public static void main(String[] args)  {
+        try {
+            DownloadIMSRReport downloadISMRReports = new DownloadIMSRReport();
+            File outputFile = downloadISMRReports.downloadReport();
+            String fqTarget = outputFile.getAbsolutePath();
+            Path outputFilePath = Paths.get(fqTarget);
+            long fqTargetSize = Files.size(outputFilePath);
+            downloadISMRReports.logger.info("Wrote " + fqTargetSize + " bytes to " + fqTarget + ".");
+        } catch (Exception e) { }
     }
 }
