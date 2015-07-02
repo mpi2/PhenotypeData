f775aa56e0b1b904a0346dfb8250d8d7b74a9c2a
jwgwarren <warren.jonathan@gmail.com>
2015-07-02 10:00:30 +0100

    put application.properties into the main projects gitignore and removed the ones already in the modules

--- data-model-solr/src/main/java/org/mousephenotype/cda/repositories/solr/parameter/ParameterDefinition.java
+++ data-model-solr/src/main/java/org/mousephenotype/cda/repositories/solr/parameter/ParameterDefinition.java
@@ -1,6 +1,11 @@
 package org.mousephenotype.cda.repositories.solr.parameter;
 
+import org.mousephenotype.cda.service.dto.GeneDTO;
+import org.mousephenotype.cda.service.dto.MpDTO;
+import org.mousephenotype.cda.service.dto.ObservationDTO;
+
 public interface ParameterDefinition {
 
-	String STABLE_ID_FIELD_NAME="parameter_stable_id";
+	
+
 }
