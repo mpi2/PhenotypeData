1de45ab636be423339af1aabdeb81444a1df149b
Jeremy Mason <jmason@ebi.ac.uk>
2015-07-09 17:06:51 +0100

    updates paths in config files

--- web/src/main/java/uk/ac/ebi/phenotype/util/SolrUtils.java
+++ web/src/main/java/uk/ac/ebi/phenotype/util/SolrUtils.java
@@ -13,7 +13,7 @@
  * language governing permissions and limitations under the
  * License.
  *******************************************************************************/
-package uk.ac.ebi.phenotype.imaging.utils;
+package uk.ac.ebi.phenotype.util;
 
 public class SolrUtils {
 
