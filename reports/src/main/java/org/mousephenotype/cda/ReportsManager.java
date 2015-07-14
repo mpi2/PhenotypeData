b39c4dc8251a123a41d6aec958cf17b4b6e90c01
Jeremy Mason <jmason@ebi.ac.uk>
2015-07-14 14:45:27 +0100

    start of report manager application

--- reports/src/main/java/org/mousephenotype/cda/ReportsManager.java
+++ reports/src/main/java/org/mousephenotype/cda/ReportsManager.java
@@ -1,4 +1,4 @@
-package org.mousephenotype.cda.reports;
+package org.mousephenotype.cda;
 
 import org.mousephenotype.cda.db.repositories.ObservationRepository;
 import org.slf4j.Logger;
@@ -9,6 +9,9 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
 
+import java.util.Arrays;
+import java.util.List;
+
 
 /**
  * Created by jmason on 23/06/2015.
@@ -16,25 +19,33 @@ import org.springframework.context.annotation.ComponentScan;
 
 @ComponentScan("org.mousephenotype.org")
 @SpringBootApplication
-public class Application implements CommandLineRunner {
+public class ReportsManager implements CommandLineRunner {
 
 	@Autowired(required=true)
 	ObservationRepository observations;
 
-	private static final Logger log = LoggerFactory.getLogger(Application.class);
+	private static final Logger log = LoggerFactory.getLogger(ReportsManager.class);
+	private static final String REPORTS_ARG = "reports";
+	private static final String TARGET_ARG = "targetDirectory";
+	private static final String PROPERTIES_FILE_ARG = "propertiesFile";
+
 
 	public static void main(String args[]) {
-		SpringApplication.run(Application.class, args);
+		SpringApplication.run(ReportsManager.class, args);
 	}
 
 
 	@Override
 	public void run(String... strings) throws Exception {
 
-		log.info("test");
-		observations.findById(1L);
-
+		List<String> options = Arrays.asList(strings);
+		if ( ! options.contains("spring.config.name")) {
+			System.out.println("No configuration file.");
+			return;
+		}
 	}
+
+
 }
 
 
