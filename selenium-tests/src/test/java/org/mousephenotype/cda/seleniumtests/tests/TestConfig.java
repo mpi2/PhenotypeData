20961e5843858e15f4d9273b185b621f5f104cf6
Jeremy Mason <jmason@ebi.ac.uk>
2015-07-15 11:12:26 +0100

    updates in order to run the selenium tests

--- selenium-tests/src/test/java/org/mousephenotype/cda/seleniumtests/tests/TestConfig.java
+++ selenium-tests/src/test/java/org/mousephenotype/cda/seleniumtests/tests/TestConfig.java
@@ -24,12 +24,63 @@ package org.mousephenotype.cda.seleniumtests.tests;
  */
 
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceBuilder;
+import org.springframework.boot.autoconfigure.orm.jpa.EntityManagerFactoryBuilder;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Primary;
+import org.springframework.jdbc.datasource.DataSourceTransactionManager;
+import org.springframework.orm.jpa.LocalContainerEntityManagerFactoryBean;
+import org.springframework.orm.jpa.vendor.HibernateJpaSessionFactoryBean;
+import org.springframework.transaction.PlatformTransactionManager;
+
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.PersistenceContext;
+import javax.sql.DataSource;
+
 
 @ComponentScan("org.mousephenotype.cda")
 @Configuration
 @EnableAutoConfiguration
 public class TestConfig {
+
+	@Bean
+	@Primary
+	@ConfigurationProperties(prefix = "datasource.komp2")
+	public DataSource komp2DataSource() {
+		return DataSourceBuilder.create().build();
+	}
+
+	@Bean
+	@Primary
+	@PersistenceContext(name="komp2Context")
+	public LocalContainerEntityManagerFactoryBean emf(EntityManagerFactoryBuilder builder){
+		return builder
+			.dataSource(komp2DataSource())
+			.packages("org.mousephenotype.cda.db")
+			.persistenceUnit("komp2")
+			.build();
+	}
+
+	@Bean(name = "sessionFactory")
+	public HibernateJpaSessionFactoryBean sessionFactory(EntityManagerFactory emf) {
+		HibernateJpaSessionFactoryBean factory = new HibernateJpaSessionFactoryBean();
+		factory.setEntityManagerFactory(emf);
+		return factory;
+	}
+
+	@Bean(name = "komp2TxManager")
+	public PlatformTransactionManager txManager() {
+		return new DataSourceTransactionManager(komp2DataSource());
+	}
+
+	@Bean
+	@ConfigurationProperties(prefix = "datasource.admintools")
+	public DataSource admintoolsDataSource() {
+		return DataSourceBuilder.create().build();
+	}
+
 
 }
