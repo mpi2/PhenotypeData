ad7f1100cdaa7ea9ef2d1b1e2fd86f064d2705e6
mrelac <mrelac@ebi.ac.uk>
2015-07-22 15:04:56 +0100

    Removed web dependencies from data-model-solr and indexer.

--- data-model-solr/src/main/java/org/mousephenotype/cda/solr/service/PhenotypeCenterService.java
+++ data-model-solr/src/main/java/org/mousephenotype/cda/solr/service/PhenotypeCenterService.java
@@ -13,6 +13,7 @@
  * language governing permissions and limitations under the
  * License.
  *******************************************************************************/
+
 package org.mousephenotype.cda.solr.service;
 
 import org.apache.solr.client.solrj.SolrQuery;
