0523ca9e8d3e1f902a829e519cb41802018d827b
mrelac <mrelac@ebi.ac.uk>
2015-07-27 17:15:54 +0100

    Refactored more tests, moving them out of 'ReportsServices' and into their own class. Also refactored many uses of ArrayList in interfaces, parameter lists, and Maps to List.

---
+++ reports/src/main/java/org/mousephenotype/cda/reports/LacZExpressionReport.java
@@ -0,0 +1,88 @@
+/*******************************************************************************
+ * Copyright © 2015 EMBL - European Bioinformatics Institute
+ * <p>
+ * Licensed under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License. You may obtain a copy of the License at
+ * http://www.apache.org/licenses/LICENSE-2.0
+ * <p>
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND,
+ * either express or implied. See the License for the specific
+ * language governing permissions and limitations under the
+ * License.
+ ******************************************************************************/
+
+package org.mousephenotype.cda.reports;
+
+import org.mousephenotype.cda.reports.support.ReportException;
+import org.mousephenotype.cda.solr.service.ImageService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.stereotype.Component;
+
+import java.io.IOException;
+import java.util.List;
+import java.util.Map;
+
+/**
+ * lac-z expression report.
+ *
+ * Created by mrelac on 24/07/2015.
+ */
+@SpringBootApplication
+@Component
+public class LacZExpressionReport extends AbstractReport {
+
+    protected Logger log = LoggerFactory.getLogger(this.getClass());
+
+    @Autowired
+    ImageService imageService;
+
+    public LacZExpressionReport() throws ReportException {
+
+    }
+
+    public static void main(String args[]) {
+        SpringApplication.run(LacZExpressionReport.class, args);
+    }
+
+    @Override
+    public String getDefaultFilename() {
+        return "laczExpressionReport";
+    }
+
+    @Override
+    public void run(String[] args) throws ReportException {
+        Map<String, String> propertyMap = parse(args);
+        List<String> errors = validate(propertyMap);
+
+        if ( ! errors.isEmpty()) {
+            for (String error : errors) {
+                System.out.println(error);
+            }
+            System.out.println();
+            usage();
+            System.exit(1);
+        }
+
+        logInputParameters();
+
+        long start = System.currentTimeMillis();
+
+        List<String[]> result = imageService.getLaczExpressionSpreadsheet();
+        csvWriter.writeAll(result);
+
+        try {
+            csvWriter.close();
+        } catch (IOException e) {
+            throw new ReportException("Exception closing csvWriter: " + e.getLocalizedMessage());
+        }
+
+        log.info(String.format("Finished. [%s]", commonUtils.msToHms(System.currentTimeMillis() - start)));
+    }
+}
