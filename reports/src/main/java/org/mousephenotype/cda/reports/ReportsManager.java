4760e92fc47c6e28ed26a1a491fdf40b35e15d2b
mrelac <mrelac@ebi.ac.uk>
2015-07-30 16:40:42 +0100

    github still shows the file as BMDStatsReport.java with class inside named BmdStatsReport. Try refactoring it to BMDStatsReport, then deleting it, then re-adding it as BmdStatsReport.

--- reports/src/main/java/org/mousephenotype/cda/reports/ReportsManager.java
+++ reports/src/main/java/org/mousephenotype/cda/reports/ReportsManager.java
@@ -39,7 +39,7 @@ import java.util.List;
 public class ReportsManager implements CommandLineRunner {
 
     @Autowired
-    BMDStatsReport bmdStatsReport;
+    BmdStatsReport bmdStatsReport;
 
     @Autowired
     BmdStatsGlucoseConcentrationReport bmdStatsGlucoseConcentrationReport;
