9a6d08636773f6b7274eaf737b5efeafbcc68696
Ilinca Tudose <ilinca.tudose@gmail.com>
2015-08-10 13:54:08 +0100

    Moved var declaration earlier

--- web/src/main/java/uk/ac/ebi/phenotype/web/controller/FileExportController.java
+++ web/src/main/java/uk/ac/ebi/phenotype/web/controller/FileExportController.java
@@ -997,8 +997,9 @@ public class FileExportController {
 			}
 
 			// ES/Mice production status
-			boolean toExport = true;
-			String genePageUrl =  request.getAttribute("mappedHostname").toString() + request.getAttribute("baseUrl").toString();			
+			boolean toExport = true;	
+			String mgiId = doc.getString(GeneDTO.MGI_ACCESSION_ID);
+			String genePageUrl = request.getAttribute("mappedHostname").toString() + request.getAttribute("baseUrl").toString() + "/genes/" + mgiId;			
 			String prodStatus = geneService.getProductionStatusForEsCellAndMice(doc, genePageUrl, toExport);
 
 			data.add(prodStatus);
@@ -1006,9 +1007,7 @@ public class FileExportController {
 			String statusField = (doc.containsKey(GeneDTO.LATEST_PHENOTYPE_STATUS)) ? doc.getString(GeneDTO.LATEST_PHENOTYPE_STATUS) : null;
 			Integer legacyPhenotypeStatus = (doc.containsKey(GeneDTO.LEGACY_PHENOTYPE_STATUS)) ? doc.getInt(GeneDTO.LEGACY_PHENOTYPE_STATUS) : null;
 			Integer hasQc = (doc.containsKey(GeneDTO.HAS_QC)) ? doc.getInt(GeneDTO.HAS_QC) : null;     
-			String mgiId = doc.getString(GeneDTO.MGI_ACCESSION_ID);
-			String genePageUrl = request.getAttribute("mappedHostname").toString() + request.getAttribute("baseUrl").toString() + "/genes/" + mgiId;			
-
+		
 			String phenotypeStatus = geneService.getPhenotypingStatus(statusField, hasQc, legacyPhenotypeStatus, genePageUrl, toExport, legacyOnly);
 
 			if (phenotypeStatus.isEmpty()) {
