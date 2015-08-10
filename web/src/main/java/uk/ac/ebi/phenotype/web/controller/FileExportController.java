2b995984c7c78942f1c7254805d6cadbe391c1a0
Ilinca Tudose <ilinca.tudose@gmail.com>
2015-08-10 13:52:35 +0100

    Removed + character

--- web/src/main/java/uk/ac/ebi/phenotype/web/controller/FileExportController.java
+++ web/src/main/java/uk/ac/ebi/phenotype/web/controller/FileExportController.java
@@ -1007,7 +1007,7 @@ public class FileExportController {
 			Integer legacyPhenotypeStatus = (doc.containsKey(GeneDTO.LEGACY_PHENOTYPE_STATUS)) ? doc.getInt(GeneDTO.LEGACY_PHENOTYPE_STATUS) : null;
 			Integer hasQc = (doc.containsKey(GeneDTO.HAS_QC)) ? doc.getInt(GeneDTO.HAS_QC) : null;     
 			String mgiId = doc.getString(GeneDTO.MGI_ACCESSION_ID);
-+			String genePageUrl = request.getAttribute("mappedHostname").toString() + request.getAttribute("baseUrl").toString() + "/genes/" + mgiId;			
+			String genePageUrl = request.getAttribute("mappedHostname").toString() + request.getAttribute("baseUrl").toString() + "/genes/" + mgiId;			
 
 			String phenotypeStatus = geneService.getPhenotypingStatus(statusField, hasQc, legacyPhenotypeStatus, genePageUrl, toExport, legacyOnly);
 
