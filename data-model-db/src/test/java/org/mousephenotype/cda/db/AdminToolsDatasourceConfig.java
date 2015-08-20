e9a3a2ca931931ca34e45d3fc7070fffa8bbca2d
Jeremy Mason <jmason@ebi.ac.uk>
2015-08-20 17:08:45 +0100

    comment out the database configuration for test

--- data-model-db/src/test/java/org/mousephenotype/cda/db/AdminToolsDatasourceConfig.java
+++ data-model-db/src/test/java/org/mousephenotype/cda/db/AdminToolsDatasourceConfig.java
@@ -1,17 +1,9 @@
 package org.mousephenotype.cda.db;
 
 import org.mousephenotype.cda.annotations.ComponentScanNonParticipant;
-import org.springframework.boot.autoconfigure.jdbc.DataSourceBuilder;
-import org.springframework.boot.autoconfigure.orm.jpa.EntityManagerFactoryBuilder;
-import org.springframework.boot.context.properties.ConfigurationProperties;
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
-import org.springframework.orm.jpa.LocalContainerEntityManagerFactoryBean;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
-
-import javax.persistence.PersistenceContext;
-import javax.sql.DataSource;
 
 /**
  * AdminToolsDatasourceConfig holds the configuration for the admintools datasource
@@ -23,20 +15,20 @@ import javax.sql.DataSource;
 @ComponentScanNonParticipant
 public class AdminToolsDatasourceConfig {
 
-	@Bean
-	@ConfigurationProperties(prefix = "datasource.admintools")
-	public DataSource admintoolsDataSource() {
-		return DataSourceBuilder.create().build();
-	}
-
-	@Bean(name="emf2")
-	@PersistenceContext(name="adminContext")
-	public LocalContainerEntityManagerFactoryBean emf2(EntityManagerFactoryBuilder builder){
-		return builder
-			.dataSource(admintoolsDataSource())
-			.packages("org.mousephenotype.cda.db")
-			.persistenceUnit("admintools")
-			.build();
-	}
+//	@Bean
+//	@ConfigurationProperties(prefix = "datasource.admintools")
+//	public DataSource admintoolsDataSource() {
+//		return DataSourceBuilder.create().build();
+//	}
+//
+//	@Bean(name="emf2")
+//	@PersistenceContext(name="adminContext")
+//	public LocalContainerEntityManagerFactoryBean emf2(EntityManagerFactoryBuilder builder){
+//		return builder
+//			.dataSource(admintoolsDataSource())
+//			.packages("org.mousephenotype.cda.db")
+//			.persistenceUnit("admintools")
+//			.build();
+//	}
 
 }
