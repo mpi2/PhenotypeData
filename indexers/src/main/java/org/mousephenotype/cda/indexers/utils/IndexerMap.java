dfa223e7125e075293b8535cf219cc0f7d44db9e
ckchen-ebi <ckchen@ebi.ac.uk>
2015-08-26 15:32:44 +0100

    fixed replace string

--- indexers/src/main/java/org/mousephenotype/cda/indexers/utils/IndexerMap.java
+++ indexers/src/main/java/org/mousephenotype/cda/indexers/utils/IndexerMap.java
@@ -84,7 +84,7 @@ public class IndexerMap {
 					String[] kv = line.split(",");
 					if ( kv.length == 2 ){
 						String mappedId = kv[0];
-						String maId = kv[1];
+						String maId = kv[1].replace("_", ":");
 					
 						if ( ! maUberonEfoMap.containsKey(maId) ){
 							maUberonEfoMap.put(maId, new HashMap<>());
