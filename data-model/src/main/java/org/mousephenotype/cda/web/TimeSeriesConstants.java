fe176134d3e78d4cddd79b5052cad58251dc1e1d
Ilinca Tudose <ilinca.tudose@gmail.com>
2015-09-10 10:45:30 +0100

    Renamed "Parameters" class to "Constants"

--- data-model/src/main/java/org/mousephenotype/cda/web/TimeSeriesConstants.java
+++ data-model/src/main/java/org/mousephenotype/cda/web/TimeSeriesConstants.java
@@ -27,7 +27,7 @@ import java.util.List;
  * Created by mrelac on 14/07/2015.
  */
 @Component
-public class TimeSeriesParameters {
+public class TimeSeriesConstants {
     public static final List<String> ESLIM_702 = Arrays.asList("ESLIM_009_001_003", "ESLIM_010_001_003", "ESLIM_011_001_011", "ESLIM_012_001_005", "ESLIM_013_001_018", "ESLIM_022_001_001");
     public static final List<String> ESLIM_701 = Arrays.asList("ESLIM_001_001_001", "ESLIM_002_001_001", "ESLIM_003_001_001", "ESLIM_004_001_001", "ESLIM_005_001_001", "ESLIM_020_001_001", "ESLIM_022_001_001");
     public static final List<String> IMPC_BWT = Arrays.asList("IMPC_GRS_003_001", "IMPC_CAL_001_001", "IMPC_DXA_001_001", "IMPC_HWT_007_001", "IMPC_PAT_049_001", "IMPC_BWT_001_001", "IMPC_ABR_001_001", "IMPC_CHL_001_001", "TCP_CHL_001_001", "HMGU_ROT_004_001");
