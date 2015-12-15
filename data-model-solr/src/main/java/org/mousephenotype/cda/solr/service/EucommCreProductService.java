6575f3d02824cae50bf565030b0ca2b129f6ebc0
jwgwarren <warren.jonathan@gmail.com>
2015-12-15 10:46:47 +0000

    new imits solr cores tests

---
+++ data-model-solr/src/main/java/org/mousephenotype/cda/solr/service/EucommCreProductService.java
@@ -0,0 +1,35 @@
+package org.mousephenotype.cda.solr.service;
+
+import org.apache.solr.client.solrj.SolrQuery;
+import org.apache.solr.client.solrj.SolrServerException;
+import org.apache.solr.client.solrj.impl.HttpSolrServer;
+import org.apache.solr.client.solrj.response.QueryResponse;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.stereotype.Service;
+
+@Service
+public class EucommCreProductService implements WebStatus{
+	
+	@Autowired @Qualifier("eucommCreProductsCore")
+	private HttpSolrServer solr;
+
+	@Override
+	public long getWebStatus() throws SolrServerException {
+
+		SolrQuery query = new SolrQuery();
+
+		query.setQuery("*:*").setRows(0);
+
+		//System.out.println("SOLR URL WAS " + solr.getBaseURL() + "/select?" + query);
+
+		QueryResponse response = solr.query(query);
+		return response.getResults().getNumFound();
+	}
+	
+	@Override
+	public String getServiceName(){
+		return "Imits Allele2 Core";
+	}
+
+}
