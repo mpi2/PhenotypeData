c221d56657aa79c01e3f0c6d3ed1c44052e6ebf6
jmason <jmason@ebi.ac.uk>
2016-01-06 16:54:34 +0000

    remove restriction on how many documents to load (load all)

--- indexers/src/main/java/org/mousephenotype/cda/indexers/ObservationIndexer.java
+++ indexers/src/main/java/org/mousephenotype/cda/indexers/ObservationIndexer.java
@@ -184,7 +184,7 @@ public class ObservationIndexer extends AbstractIndexer {
                 + "LEFT OUTER JOIN image_record_observation iro ON o.id=iro.id "
                 + "INNER JOIN experiment_observation eo ON eo.observation_id=o.id "
                 + "INNER JOIN experiment e on eo.experiment_id=e.id "
-                + "WHERE o.missing=0 AND e.id in (46814, 46815, 46816)";
+                + "WHERE o.missing=0";
 
         try (PreparedStatement p = connection.prepareStatement(query, java.sql.ResultSet.TYPE_FORWARD_ONLY, java.sql.ResultSet.CONCUR_READ_ONLY)) {
 
