e224386cc1a51646422c223bbb5020eca96f663a
mrelac <mrelac@ebi.ac.uk>
2016-01-11 17:52:15 +0000

    Fixed OmeroStatusService to fetch the impcMediaBaseUrl as a value from the properties file rather than from globalConfiguration, which is not accessible to the solr services.

--- data-model-solr/src/main/java/org/mousephenotype/cda/solr/service/OmeroStatusService.java
+++ data-model-solr/src/main/java/org/mousephenotype/cda/solr/service/OmeroStatusService.java
@@ -1,62 +1,60 @@
 package org.mousephenotype.cda.solr.service;
 
+import org.apache.solr.client.solrj.SolrServerException;
+import org.mousephenotype.cda.web.WebStatus;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Service;
+
+import javax.annotation.PostConstruct;
+import javax.validation.constraints.NotNull;
 import java.io.IOException;
 import java.net.HttpURLConnection;
 import java.net.MalformedURLException;
 import java.net.ProtocolException;
 import java.net.URL;
-import java.util.Map;
-
-import javax.annotation.PostConstruct;
-import javax.annotation.Resource;
-
-import org.apache.solr.client.solrj.SolrServerException;
-import org.mousephenotype.cda.web.WebStatus;
-import org.springframework.stereotype.Service;
 
 @Service
-public class OmeroStatusService implements WebStatus{
-	
-	 @Resource(name = "globalConfiguration")
-	  private Map<String, String> config;
-	 private String omeroUrl;
+public class OmeroStatusService implements WebStatus {
 
-	 @PostConstruct
-	 public void initisialise(){
-		 
-		 omeroUrl=config.get("impcMediaBaseUrl");
-		 System.out.println("initialised OmeroStatus Service with url="+omeroUrl);
-	 }
+    @NotNull
+    @Value("${impcMediaBaseUrl}")
+    private String omeroUrl;
 
-	@Override
-	public long getWebStatus() throws SolrServerException {
-		String url="http:"+omeroUrl+"/render_thumbnail/1/";
-		
-		try {
-			URL obj = new URL(url);
-			HttpURLConnection con = (HttpURLConnection) obj.openConnection();
-			con.setRequestMethod("GET");
-			con.setRequestProperty("User-Agent", "OmeroStatus request from PhenotypeArchive");
-			int responseCode = con.getResponseCode();
-			if(responseCode!=200){
-				return 0;
-			}
-		} catch (MalformedURLException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		} catch (ProtocolException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		} catch (IOException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
-		return 1;//if all is well return 1 to indicate service is up.
-	}
-	
-	@Override
-	public String getServiceName(){
-		return "OmeroStatus Service";
-	}
+    @PostConstruct
+    public void initisialise() {
+
+        System.out.println("initialised OmeroStatus Service with url=" + omeroUrl);
+    }
+
+    @Override
+    public long getWebStatus() throws SolrServerException {
+        String url = "http:" + omeroUrl + "/render_thumbnail/1/";
+
+        try {
+            URL obj = new URL(url);
+            HttpURLConnection con = (HttpURLConnection) obj.openConnection();
+            con.setRequestMethod("GET");
+            con.setRequestProperty("User-Agent", "OmeroStatus request from PhenotypeArchive");
+            int responseCode = con.getResponseCode();
+            if (responseCode != 200) {
+                return 0;
+            }
+        } catch (MalformedURLException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        } catch (ProtocolException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        } catch (IOException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        }
+        return 1;//if all is well return 1 to indicate service is up.
+    }
+
+    @Override
+    public String getServiceName() {
+        return "OmeroStatus Service";
+    }
 
 }
