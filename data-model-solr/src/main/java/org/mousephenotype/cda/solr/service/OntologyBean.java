1a210d7473c250a0c63a080c05311a64534cd658
jwgwarren <warren.jonathan@gmail.com>
2016-03-09 16:53:03 +0000

    hitopath table diplaying

--- data-model-solr/src/main/java/org/mousephenotype/cda/solr/service/OntologyBean.java
+++ data-model-solr/src/main/java/org/mousephenotype/cda/solr/service/OntologyBean.java
@@ -15,6 +15,11 @@ public class OntologyBean{
 
 	}
 
+	public OntologyBean(String id, String name, String description) {
+		this(id, name);
+		this.description=description;
+	}
+
 	String id;
 	public String getId() {
 		return id;
