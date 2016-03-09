e2fafb8ea73d3e842ada292ab633c5a26dbd0ad5
mrelac <mrelac@ebi.ac.uk>
2016-03-09 14:42:38 +0000

    Removed dependency on maven-project-info-reports-plugin from parent pom, JSON dependencies in data-model-solr pom, added latest release of spring-boot-starter-data-solr to data-model-solr pom, removed all references to org.apache.commons.lang, replacing them with org.apache.commons.lang3, and added dependency on commons-collections 3.2.2 in data-model pom to avoid the use of v3.2.1, which has a security issue.

--- data-model-db/src/main/java/org/mousephenotype/cda/db/dao/HpOntologyDAO.java
+++ data-model-db/src/main/java/org/mousephenotype/cda/db/dao/HpOntologyDAO.java
@@ -16,7 +16,7 @@
 
 package org.mousephenotype.cda.db.dao;
 
-import org.apache.commons.lang.StringUtils;
+import org.apache.commons.lang3.StringUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
