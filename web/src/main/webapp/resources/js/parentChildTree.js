7726776594800cca97001e529faa0e773368cb6e
Ilinca Tudose <ilinca.tudose@gmail.com>
2016-03-30 18:18:05 +0100

    Fixed duplicate nodes for current page in parent child display, phen page.

--- web/src/main/webapp/resources/js/parentChildTree.js
+++ web/src/main/webapp/resources/js/parentChildTree.js
@@ -1,4 +1,4 @@
-        var width = 260,
+        var width = 222,
         height = 200;
         
 	    var cluster = d3.layout.cluster()
@@ -19,67 +19,21 @@
 	   	    x: function(d) { return d.y * 100 / width; },
 	 	    y: function(d) { return d.x; }
 	    };
+
+	    d3.select("#parentChild").style("width", width*2 + "px");
+	    d3.select("#childDiv").style("width", width + "px");
+	    d3.select("#parentDiv").style("width", width + "px");
+	
 	    
-	
-	    d3.json("../mpTree/json/" + mp_id + "?type=children", function(error, root) {
-	    	  
-		    var svg = d3.select("#childDiv").append("svg")
-		        .attr("width", width)
-		        .attr("height", height)
-		        .append("g")
-		        .attr("transform", "translate(5,0)");
-	    	
-		     if (error){ 
-		    	  console.log(error);
-		    	  throw error;
-		   	 }
-		
-		     if (root.children.length == 0){
-		    		hasParents = false;
-		    		d3.select("#childDiv").remove();
-		    		d3.selectAll("#parentDiv")
-		    		  .classed("half", false);
-		  	 }
-		     
-		     var nodes = cluster.nodes(root),
-		          links = cluster.links(nodes);
-		
-		     var link = svg.selectAll(".link")
-		          .data(links)
-		          .enter().append("path")
-		          .attr("class", "link")
-		          .attr("d", d3.svg.diagonal().projection(function(d) { return [orientation_right.x(d), orientation_right.y(d)]; }));
-	
-		     var node = svg.selectAll(".node")
-		          .data(nodes)
-		          .enter().append("g")
-		          .attr("class", "node")
-		          .attr("transform", function(d) { return "translate(" +  orientation_right.x(d) + "," + orientation_right.y(d) + ")"; })
-		          
-		     node.append("circle")
-		          .attr("r", 4.5)
-		          .style("fill", function(d) { return d.children ? "lightsteelblue" : "#fff"; });
-		
-		     node.append("text")
-		          .attr("x", function(d) { return d.children ? -8 : 8; })
-		          .attr("y", 3)
-		          .attr("class", "treeLabel")
-		          .style("text-anchor", function(d) { return d.children ? "end" : "start"; })
-		          .text(function(d) { return d.name; })
-	   		      .style("width", "150px")
-	   		      .on('click', function(d, i) {
-					  window.location.href = "../phenotypes/"  + d.id;
-				   }); // width of the node labels; 
-	    });
-	     
-	     
+ var svgP;
+	    
 	    d3.json("../mpTree/json/" + mp_id + "?type=parents", function(error, root) {
 	    	
-	    	 var svgP = d3.select("#parentDiv").append("svg")
+	    	svgP = d3.select("#parentDiv").append("svg")
 		        .attr("width", width)
 		        .attr("height", height)
 		        .append("g")
-		        .attr("transform", "translate(95,0)");
+		        .attr("transform", "translate(120,0)");
 	    	 
 		     var diagonalP = d3.svg.diagonal()
 	        	.projection(function(d) { return [d.y, d.x]; });
@@ -132,5 +86,61 @@
 		     
 		     
 	    });
+	    
+	    
+	    d3.json("../mpTree/json/" + mp_id + "?type=children", function(error, root) {
+	    	  
+		    var svg = d3.select("#childDiv").append("svg")
+		        .attr("width", width)
+		        .attr("height", height)
+		        .append("g")
+		        .attr("transform", "translate(5,0)");
+	    	
+		     if (error){ 
+		    	  console.log(error);
+		    	  throw error;
+		   	 }
+		
+		     if (root.children.length == 0){
+		    		hasParents = false;
+		    		d3.select("#childDiv").remove();
+		    		d3.selectAll("#parentDiv")
+		    		  .classed("half", false);
+
+			        svgP.attr("transform", "translate(100,0)");
+		  	 }
+		     
+		     var nodes = cluster.nodes(root),
+		          links = cluster.links(nodes);
+		
+		     var link = svg.selectAll(".link")
+		          .data(links)
+		          .enter().append("path")
+		          .attr("class", "link")
+		          .attr("d", d3.svg.diagonal().projection(function(d) { return [orientation_right.x(d), orientation_right.y(d)]; }));
+	
+		     var node = svg.selectAll(".node")
+		          .data(nodes)
+		          .enter().append("g")
+		          .attr("class", "node")
+		          .attr("transform", function(d) { return "translate(" +  orientation_right.x(d) + "," + orientation_right.y(d) + ")"; })
+		          
+		     node.append("circle")
+		          .attr("r", 4.5)
+		          .style("fill", function(d) { return d.children ? "lightsteelblue" : "#fff"; });
+		
+		     node.append("text")
+		          .attr("x", function(d) { return d.children ? -8 : 8; })
+		          .attr("y", 3)
+		          .attr("class", "treeLabel")
+		          .style("text-anchor", function(d) { return d.children ? "end" : "start"; })
+		          .text(function(d) { return d.name; })
+	   		      .style("width", "150px")
+	   		      .on('click', function(d, i) {
+					  window.location.href = "../phenotypes/"  + d.id;
+				   }); // width of the node labels; 
+	    });
+	     
+	   
 	    d3.select(self.frameElement).style("height", height + "px");
 	    
