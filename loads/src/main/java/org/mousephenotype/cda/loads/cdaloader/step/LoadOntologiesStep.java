dd99a39e86ba74e071b504241a70657595f365ab
mrelac <mrelac@ebi.ac.uk>
2016-04-28 15:15:40 +0100

    Refactored OboItemReader. It still doesn't work properly, but it does copy records line by line. Contiuing to investigate multi-line record processing.

--- loads/src/main/java/org/mousephenotype/cda/loads/cdaloader/step/LoadOntologiesStep.java
+++ loads/src/main/java/org/mousephenotype/cda/loads/cdaloader/step/LoadOntologiesStep.java
@@ -16,46 +16,38 @@
 
 package org.mousephenotype.cda.loads.cdaloader.step;
 
-import org.mousephenotype.cda.db.pojo.DatasourceEntityId;
 import org.mousephenotype.cda.db.pojo.OntologyTerm;
-import org.mousephenotype.cda.utilities.CommonUtils;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.batch.core.configuration.annotation.StepScope;
-import org.springframework.batch.item.ItemReader;
 import org.springframework.batch.item.ItemWriter;
-import org.springframework.batch.item.file.FlatFileItemReader;
 import org.springframework.batch.item.file.FlatFileItemWriter;
-import org.springframework.batch.item.file.mapping.DefaultLineMapper;
-import org.springframework.batch.item.file.mapping.PassThroughFieldSetMapper;
-import org.springframework.batch.item.file.transform.*;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Value;
+import org.springframework.batch.item.file.transform.DelimitedLineTokenizer;
+import org.springframework.batch.item.file.transform.FieldSet;
+import org.springframework.batch.item.file.transform.PassThroughLineAggregator;
 import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.PropertySource;
 import org.springframework.core.io.FileSystemResource;
-import org.springframework.util.Assert;
 
-import java.util.HashMap;
 import java.util.List;
 
 /**
  * Created by mrelac on 26/04/16.
  */
 @Configuration
-@ComponentScan("org.mousephenotype.cda.loads.cdaloader")
-@PropertySource(value="file:${user.home}/configfiles/${profile}/application.properties")
-@PropertySource(value="file:${user.home}/configfiles/${profile}/cdaload.properties",
-                ignoreResourceNotFound=true)
-public class LoadOntologiesStep implements ItemReader<OntologyTerm>, ItemWriter<OntologyTerm> {
+//@ComponentScan("org.mousephenotype.cda.loads.cdaloader")
+//@PropertySource(value="file:${user.home}/configfiles/${profile}/application.properties")
+//@PropertySource(value="file:${user.home}/configfiles/${profile}/cdaload.properties",
+//                ignoreResourceNotFound=true)
+public class LoadOntologiesStep implements ItemWriter<OntologyTerm> {
 
-    CommonUtils commonUtils = new CommonUtils();
-    private final Logger logger = LoggerFactory.getLogger(this.getClass());
+//    CommonUtils commonUtils = new CommonUtils();
+//    private final Logger logger = LoggerFactory.getLogger(this.getClass());
+//
+//    @Value("${cdaload.workspace}")
+//    private String cdaWorkspace;
 
-    @Value("${cdaload.workspace}")
-    private String cdaWorkspace;
+//    @Autowired
+//    @Qualifier("oboReaderBean")
+//    public ItemReader<OntologyTerm> oboReader;
 
 //    @Autowired
 //    DataSource komp2loads;
@@ -77,90 +69,17 @@ public class LoadOntologiesStep implements ItemReader<OntologyTerm>, ItemWriter<
 //
 //    @Value("${cdaload.dbport}")
 //    private String dbport;
-//
-//    public final long TASKLET_TIMEOUT = 10000;                                  // Timeout in milliseconds
 
-    @Autowired
-    DelimitedLineTokenizer delimitedLineTokenizer;
-
-    @Bean(name = "ontologyReader")
-    @StepScope
-    public FlatFileItemReader ontologyReader() {
-        FlatFileItemReader<FieldSet> reader = new FlatFileItemReader<>();
-
-        String sourcePath = cdaWorkspace + "/EMAP.obo";
-System.out.println("sourcePath: '" + sourcePath + "'");
-
-        reader.setResource(new FileSystemResource(sourcePath));
-        reader.setLineMapper(new DefaultLineMapper<FieldSet>() {{
-            setLineTokenizer(new PatternMatchingCompositeLineTokenizer() {{
-                setTokenizers(
-                    new HashMap<String, LineTokenizer>() {{
-                        put("[Term]", delimitedLineTokenizer);
-                        put("[Typedef]", delimitedLineTokenizer);
-
-                        put("comment: *", delimitedLineTokenizer);
-                        put("def: *", delimitedLineTokenizer);
-                        put("domain: *", delimitedLineTokenizer);
-                        put("id: *", delimitedLineTokenizer);
-                        put("name: *", delimitedLineTokenizer);
-                        put("range: *", delimitedLineTokenizer);
-                        put("relationship: *", delimitedLineTokenizer);
-                        put("synonym: *", delimitedLineTokenizer);
-                        put("*", delimitedLineTokenizer);
-                    }});
-            }});
-            setFieldSetMapper(new PassThroughFieldSetMapper());
-        }});
-        return reader;
-    }
 
     @Bean(name = "ontologyWriter")
     @StepScope
     public FlatFileItemWriter ontologyWriter() {
         FlatFileItemWriter<FieldSet> writer = new FlatFileItemWriter<>();
         writer.setLineAggregator(new PassThroughLineAggregator());
-        String sourcePath = cdaWorkspace + "/loadOntologies.log";
+        String sourcePath = "/tmp/loadOntologies.log";
         writer.setResource(new FileSystemResource(sourcePath));
 
         return writer;
-    }
-
-    @Autowired
-    FlatFileItemReader delegate;
-
-    @Override
-    public OntologyTerm read() throws Exception {
-        OntologyTerm term = null;
-
-        for (FieldSet line; (line = (FieldSet)delegate.read()) != null;) {
-            String prefix = line.readString(0);
-
-            switch (prefix.trim()) {
-                case "[Term]":
-                    term = new OntologyTerm();
-                    break;
-
-                case "id:":
-                    Assert.notNull(term, "Header is missing!");
-                    term.setId(new DatasourceEntityId(line.readString(0), -1));
-                    break;
-
-                case "name:":
-                    Assert.notNull(term, "Header is missing!");
-                    term.setName(line.readString(0));
-                    break;
-
-                case "relationship:":
-                    Assert.notNull(term, "Header is missing!");
-                    break;
-
-                default:
-                    throw new Exception("Unknown ontology term '" + prefix.trim() + "'");
-            }
-        }
-
-        return term;
     }
 
     @Bean
