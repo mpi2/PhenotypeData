afb29cbcce32c1ffe044831d8db10582e493a993
mrelac <mrelac@ebi.ac.uk>
2016-05-05 17:36:27 +0100

    Removed the colon in the prefix. The OntologyParser does not want it.

--- loads/src/main/java/org/mousephenotype/cda/loads/cdaloader/steps/tasklets/Downloader.java
+++ loads/src/main/java/org/mousephenotype/cda/loads/cdaloader/steps/tasklets/Downloader.java
@@ -23,7 +23,6 @@ import org.springframework.batch.core.StepContribution;
 import org.springframework.batch.core.scope.context.ChunkContext;
 import org.springframework.batch.core.step.tasklet.SystemCommandTasklet;
 import org.springframework.batch.repeat.RepeatStatus;
-import org.springframework.stereotype.Component;
 
 import javax.annotation.PostConstruct;
 import java.io.FileOutputStream;
@@ -43,7 +42,6 @@ import java.util.Date;
  * Created by mrelac on 13/04/2016.
  *
  */
-@Component
 public class Downloader extends SystemCommandTasklet {
 
     private String sourceUrl;
