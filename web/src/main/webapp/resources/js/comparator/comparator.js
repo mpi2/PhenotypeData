a4cc3499249fa411fe96022582d0c17701a8631f
jwgwarren <warren.jonathan@gmail.com>
2016-05-17 17:52:12 +0100

    image detail view working with js to change src att

---
+++ web/src/main/webapp/resources/js/comparator/comparator.js
@@ -0,0 +1,7 @@
+$(document).ready(function(){						
+	
+console.log('comparator.js ready');
+$('#control').attr('src','http://www.ebi.ac.uk/mi/media/omero/webgateway/img_detail/10/');
+//$('#mutant').load('http://www.ebi.ac.uk/mi/media/omero/webgateway/img_detail/89929/');
+
+});
