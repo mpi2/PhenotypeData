593d1a85fc5b2979fb5fd62f1f5047bd17c50e62
mrelac <mrelac@ebi.ac.uk>
2016-06-13 18:54:26 +0100

    Final cut at MarkerProcessorGeneTypes.

--- loads/src/main/java/org/mousephenotype/cda/loads/cdaloader/steps/MarkerProcessorGeneTypes.java
+++ loads/src/main/java/org/mousephenotype/cda/loads/cdaloader/steps/MarkerProcessorGeneTypes.java
@@ -16,29 +16,36 @@
 
 package org.mousephenotype.cda.loads.cdaloader.steps;
 
+import org.mousephenotype.cda.db.pojo.DatasourceEntityId;
 import org.mousephenotype.cda.db.pojo.GenomicFeature;
+import org.mousephenotype.cda.db.pojo.OntologyTerm;
 import org.mousephenotype.cda.db.pojo.SequenceRegion;
+import org.mousephenotype.cda.enumerations.DbIdType;
 import org.mousephenotype.cda.loads.cdaloader.exceptions.CdaLoaderException;
+import org.mousephenotype.cda.loads.cdaloader.support.SqlLoaderUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.batch.item.ItemProcessor;
 import org.springframework.batch.item.file.transform.FieldSet;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 
 import java.util.HashMap;
 import java.util.HashSet;
+import java.util.Map;
 import java.util.Set;
 
 /**
  * Created by mrelac on 09/06/16.
  */
-class MarkerProcessorGeneTypes implements ItemProcessor<FieldSet, GenomicFeature> {
+public class MarkerProcessorGeneTypes implements ItemProcessor<FieldSet, GenomicFeature> {
 
-    private       int                             addedGenomicFeaturesCount = 0;
-    public final  Set<String>                     errMessages               = new HashSet<>();
-//    private      HashMap<String, OntologyTerm>   featureTypes              = null;
-    private       int                             lineNumber                = 0;
-    private final Logger                          logger                    = LoggerFactory.getLogger(this.getClass());
-    private       HashMap<String, SequenceRegion> sequenceRegions           = null;
+    public final  Set<String>                 errMessages     = new HashSet<>();
+    private       Map<String, OntologyTerm>   featureTypes    = null;
+    private       Map<String, GenomicFeature> genomicFeatures = new HashMap<>();
+    private       int                         lineNumber      = 0;
+    private final Logger                      logger          = LoggerFactory.getLogger(this.getClass());
+    private       Map<String, SequenceRegion> sequenceRegions = null;
 
     // The following ints define the column offset of the given column in the GENE_TYPES file.
     public final static int OFFSET_SEQ_REGION = 0;
@@ -48,109 +55,128 @@ class MarkerProcessorGeneTypes implements ItemProcessor<FieldSet, GenomicFeature
     public final static int OFFSET_STRAND     = 6;
     public final static int OFFSET_COMMENTS   = 8;
 
+    @Autowired
+    @Qualifier("sqlLoaderUtils")
+    private SqlLoaderUtils sqlLoaderUtils;
+
 
     @Override
     public GenomicFeature process(FieldSet item) throws Exception {
 
         lineNumber++;
 
-        HashMap<String, GenomicFeature> genomicFeatures = new HashMap<>();
+        if (featureTypes == null) {
+            featureTypes = sqlLoaderUtils.getOntologyTerms(sqlLoaderUtils.getJdbcTemplate(), DbIdType.Genome_Feature_Type.intValue());
+            sequenceRegions = sqlLoaderUtils.getSequenceRegions(sqlLoaderUtils.getJdbcTemplate());
+        }
 
-        String         accessionId;
         GenomicFeature feature   = null;
-        String         comments  = item.getValues()[OFFSET_COMMENTS];
-        String         seqRegion = item.getValues()[OFFSET_SEQ_REGION];
-        String         biotype   = item.getValues()[OFFSET_BIO_TYPE];
-        String         start     = item.getValues()[OFFSET_START];
-        String         end       = item.getValues()[OFFSET_END];
-        int            strand    = (item.getValues()[OFFSET_STRAND].equals("+") ? 1 : -1);
+
+        /*
+        * Fields within MGI_GTGUP.gff:
+        *   [0] - sequenceRegion
+        *   [1] - source (not used)
+        *   [2] - biotype
+        *   [3] - start
+        *   [4] - end
+        *   [5] - score (not used)
+        *   [6] - strand
+        *   [7] - phase (not used)
+        *   [8] - comments
+         */
 
         ParsedComment parsedComment = null;
         try {
             parsedComment = new ParsedComment(item.getValues()[OFFSET_COMMENTS]);
-        } catch (CdaLoaderException e) {
-            logger.error(e.getLocalizedMessage());
+        } catch (Exception e) {
+            logger.error(e.getLocalizedMessage() + ".\t Line number " + lineNumber);
+            return null;
         }
 
-        accessionId = parsedComment.getId();
+        String         accessionId    = parsedComment.getId();
+        String         biotype        = item.getValues()[OFFSET_BIO_TYPE];
+        String         end            = item.getValues()[OFFSET_END];
+        SequenceRegion sequenceRegion = sequenceRegions.get(item.getValues()[OFFSET_SEQ_REGION].substring(3));
+        String         start          = item.getValues()[OFFSET_START];
+        int            strand         = (item.getValues()[OFFSET_STRAND].equals("+") ? 1 : -1);
+        OntologyTerm   subtypeTerm    = featureTypes.get(parsedComment.getNote() != null ? parsedComment.getNote() : "unknown");
+        String         symbol         = parsedComment.getName();
 
         if ( ! biotype.equals("GeneModel")) {
 
             feature = new GenomicFeature();
 
+            DatasourceEntityId dsId = new DatasourceEntityId();
+            dsId.setAccession(accessionId);
+            dsId.setDatabaseId(DbIdType.MGI.intValue());
 
+            feature.setId(dsId);
+            feature.setBiotype(featureTypes.get(biotype));
+            feature.setEnd(Integer.parseInt(end));
+            feature.setSequenceRegion(sequenceRegion);
+            feature.setStart(Integer.parseInt(start));
+            feature.setStrand(strand);
+            feature.setSubtype(subtypeTerm);
+            feature.setSymbol(symbol);
 
-
-
-
-
-
-
-//            DatasourceEntityId dsId = new DatasourceEntityId();
-//            dsId.setAccession(accessionId);
-//            dsId.setDatabaseId(DbIdType.MGI.intValue());
-//            feature.setId(dsId);
-//
-//            OntologyTerm biotypeTerm = featureTypes.get(biotype); //ontologyTermDAO.getOntologyTermByName(biotype);
-//            OntologyTerm subtypeTerm = featureTypes.get(properties.get("Note")); //ontologyTermDAO.getOntologyTermByName(properties.get("Note"));
-//
-//            if (subtypeTerm == null) {
-//                subtypeTerm = featureTypes.get("unknown");
-//            }
-//
-//            feature.setBiotype(biotypeTerm);
-//            feature.setSubtype(subtypeTerm);
-//
-//            feature.setSymbol(properties.get("Name"));
-//
-//            SequenceRegion r = sequenceRegions.get(seqRegion);
-//            feature.setSequenceRegion(r);
-//
-//            feature.setStart(Integer.parseInt(start));
-//            feature.setEnd(Integer.parseInt(end));
-//
-//            feature.setStrand(strand);
-//
-//            genomicFeatures.put(accessionId, feature);
-            addedGenomicFeaturesCount++;
-
+            genomicFeatures.put(accessionId, feature);
         }
 
         return feature;
     }
 
+    public Set<String> getErrMessages() {
+        return errMessages;
+    }
 
-    // PRIVATE CLASSES
+    public Map<String, GenomicFeature> getGenomicFeatures() {
+        return genomicFeatures;
+    }
+
+// PRIVATE CLASSES
 
 
     private class ParsedComment {
         private String id = null;
         private String name = null;
         private String note = null;
-        
+
+        /*
+        * Lines come in the form "ID=MGIxxxxxxx;Name=yyyyy;Note=zzzzz"
+        * ID is on every line in the file. Name and Note are optional. The three tokens always seem to appear in the
+        * same order: ID (always), then Name (if it exists), then Note (if it exists). You can't split on ";" because
+        * some names contain ";", and a Note might as well (though I haven't seen one).
+        *
+        * Extract the ID=MGI:xxxxxxx and remove it from the string.
+         */
         public ParsedComment(String comment) throws CdaLoaderException {
             if ((comment == null) || (comment.isEmpty()))
                 return;
 
-            String[] parts = comment.split(";");
-            for (String part : parts) {
-                String[] innerParts = part.split("=");
+            String tmp = comment;
+            int endIndex = tmp.indexOf(";");
 
-                switch (innerParts[0]) {
-                    case "ID":
-                        id = innerParts[1];
-                        break;
+            if (endIndex == -1) {
+                id = tmp.substring(3);
+                return;
+            }
+            id = tmp.substring(3, endIndex);
 
-                    case "Name":
-                        name = innerParts[1];
-                        break;
+            tmp = tmp.replace("ID=" + id, "");
+            if (tmp.isEmpty()) {
+                return;
+            }
 
-                    case "Note":
-                        note = innerParts[1];
-                        break;
-
-                    default:
-                        throw new CdaLoaderException("Unexpected comment '" + comment + "'.  token '" + innerParts[0] + "' with value '" + innerParts[1] + "' in MGI_GTUP.gff");
+            if (tmp.startsWith(";Name=")) {
+                tmp = tmp.replace(";Name=", "");
+                endIndex = tmp.indexOf(";Note=");
+                if (endIndex == -1) {
+                    name = tmp;
+                    return;
+                } else {
+                    name = tmp.substring(0, endIndex);
+                    tmp = tmp.replace(name + ";Note=", "");
+                    note = tmp;
                 }
             }
         }
