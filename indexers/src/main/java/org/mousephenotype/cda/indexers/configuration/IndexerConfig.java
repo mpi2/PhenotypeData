15a2f5c7cb178a27583cb25d9b10161455224b4f
jmason <jmason@ebi.ac.uk>
2016-06-23 16:12:16 +0100

    use Mysql instead of H2; remove loading sql file before running

--- indexers/src/main/java/org/mousephenotype/cda/indexers/configuration/IndexerConfig.java
+++ indexers/src/main/java/org/mousephenotype/cda/indexers/configuration/IndexerConfig.java
@@ -160,12 +160,10 @@ public class IndexerConfig {
     protected Properties buildHibernateProperties() {
 	    Properties hibernateProperties = new Properties();
 
-	    hibernateProperties.setProperty("hibernate.dialect", "org.hibernate.dialect.H2Dialect");
-	    hibernateProperties.put("hibernate.hbm2ddl.import_files", "sql/test-data.sql");
+	    hibernateProperties.setProperty("hibernate.dialect", "org.hibernate.dialect.MySQLDialect");
 	    hibernateProperties.setProperty("hibernate.show_sql", "false");
 	    hibernateProperties.setProperty("hibernate.use_sql_comments", "true");
 	    hibernateProperties.setProperty("hibernate.format_sql", "true");
-	    hibernateProperties.setProperty("hibernate.hbm2ddl.auto", "create-drop");
 	    hibernateProperties.setProperty("hibernate.generate_statistics", "false");
 	    hibernateProperties.setProperty("hibernate.current_session_context_class", "thread");
 
