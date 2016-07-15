8f0cef9732f5f85e00c000b250adfcd0a62c746e
jwgwarren <warren.jonathan@gmail.com>
2016-07-15 11:38:32 +0100

    order table started being filled by real data

--- data-model-solr/src/main/java/org/mousephenotype/cda/solr/service/OrderService.java
+++ data-model-solr/src/main/java/org/mousephenotype/cda/solr/service/OrderService.java
@@ -1,7 +1,9 @@
 package org.mousephenotype.cda.solr.service;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 import org.apache.solr.client.solrj.SolrQuery;
 import org.apache.solr.client.solrj.SolrServerException;
@@ -35,13 +37,30 @@ public class OrderService {
 	
    
 	
-	public List<OrderTableRow> getOrderTableRows(String acc) {
+	public List<OrderTableRow> getOrderTableRows(String acc) throws SolrServerException {
 		List<OrderTableRow> orderTableRows=new ArrayList<>();
+		
+		List<Allele2DTO> allele2DTOs = this.getAllele2DTOs(acc);
+		Map<String,List<ProductDTO>> alleleNameToProductsMap=this.getProducts(acc);
+		//
+		for(Allele2DTO allele: allele2DTOs){
+			OrderTableRow row=new OrderTableRow();
+			String alleleName=allele.getAlleleName();
+			row.setAlleleName(alleleName);
+			row.setAlleleDescription(allele.getAlleleDescription());
+			
+			//row.setNoProductInfo(allele.get);
+			List<ProductDTO> productsForAllele = alleleNameToProductsMap.get(alleleName);
+			orderTableRows.add(row);
+			
+		}
+		
+		
 		
 		OrderTableRow row=new OrderTableRow();
 		row.setAlleleName("Cpsf3tm1b(EUCOMM)Wtsi");
 		row.setStrainOfOrigin("C57BL/6N");
-		row.setAlleleType("Reporter-tagged deletion allele (post-Cre)");
+		row.setAlleleDescription("Reporter-tagged deletion allele (post-Cre)");
 		GeneTargetDetail detail=new GeneTargetDetail();
 		detail.setLabel("Target Vector Map");
 		detail.setLink("https://www.i-dcc.org/imits/targ_rep/alleles/4973/allele-image-cre?simple=true.jpg.jpg");
@@ -52,36 +71,43 @@ public class OrderService {
 		return orderTableRows;
 	}
 	
-	public List<Allele2DTO> getAllele2DTOs(String geneAcc) throws SolrServerException{
+	protected List<Allele2DTO> getAllele2DTOs(String geneAcc) throws SolrServerException{
 		
-		String q = "type:allele AND mgi_accession_id:\"" + geneAcc+ "\"";//&start=0&rows=100&hl=true&wt=json";
+		String q = "mgi_accession_id:\"" + geneAcc+ "\"";//&start=0&rows=100&hl=true&wt=json";
 		SolrQuery query=new SolrQuery();
 		query.setQuery(q);
+		query.addFilterQuery("type:Allele");
 		query.setRows(Integer.MAX_VALUE);
-		System.out.println("query for products="+query);
-		QueryResponse response = productCore.query(query);
+		System.out.println("query for alleles="+query);
+		QueryResponse response = allele2Core.query(query);
 		System.out.println("number found of allele2 docs="+response.getResults().getNumFound());
 		List<Allele2DTO> allele2DTOs = response.getBeans(Allele2DTO.class);
-		System.out.println("number of products is "+allele2DTOs.size());
+		System.out.println("number of alleles is "+allele2DTOs.size());
 		
         return allele2DTOs;
 
         
 	}
 	
-public List<ProductDTO> getProducts(String geneAcc) throws SolrServerException{
-		
+	protected Map<String,List<ProductDTO>> getProducts(String geneAcc) throws SolrServerException{
+		Map<String,List<ProductDTO>> alleleNameToProductsMap=new HashMap<>();
 		String q = "mgi_accession_id:\"" + geneAcc+ "\"";//&start=0&rows=100&hl=true&wt=json";
 		SolrQuery query=new SolrQuery();
 		query.setQuery(q);
 		query.setRows(Integer.MAX_VALUE);
-		System.out.println("query for allele2="+query);
+		System.out.println("query for products="+query);
 		QueryResponse response = productCore.query(query);
-		System.out.println("number found of allele2 docs="+response.getResults().getNumFound());
+		System.out.println("number found of products docs="+response.getResults().getNumFound());
 		List<ProductDTO> productDTOs = response.getBeans(ProductDTO.class);
-		System.out.println("number of allele2DTOs is "+productDTOs.size());
+		System.out.println("number of productDTOs is "+productDTOs.size());
+		for(ProductDTO prod: productDTOs){
+			if(!alleleNameToProductsMap.containsKey(prod.getAlleleName())){
+				alleleNameToProductsMap.put(prod.getAlleleName(), new ArrayList<>());
+			}
+			alleleNameToProductsMap.get(prod.getAlleleName()).add(prod);
+		}
 		
-        return productDTOs;
+        return alleleNameToProductsMap;
 
         
 	}
