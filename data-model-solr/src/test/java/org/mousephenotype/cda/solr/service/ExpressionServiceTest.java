0ea6705108794487d8c903c99fb24e29ee5e8298
jmason <jmason@ebi.ac.uk>
2016-07-20 16:04:13 +0100

    clean up code and organise imports

--- data-model-solr/src/test/java/org/mousephenotype/cda/solr/service/ExpressionServiceTest.java
+++ data-model-solr/src/test/java/org/mousephenotype/cda/solr/service/ExpressionServiceTest.java
@@ -1,48 +1,21 @@
 package org.mousephenotype.cda.solr.service;
 
 import org.apache.solr.client.solrj.SolrServerException;
-import org.apache.solr.client.solrj.impl.HttpSolrServer;
-import org.apache.solr.client.solrj.response.QueryResponse;
-import org.hibernate.SessionFactory;
+import org.apache.solr.client.solrj.response.FacetField.Count;
 import org.junit.Test;
 import org.junit.runner.RunWith;
-import org.mousephenotype.cda.db.dao.PhenotypePipelineDAO;
-import org.mousephenotype.cda.db.dao.PhenotypePipelineDAOImpl;
 import org.mousephenotype.cda.solr.TestConfigSolr;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Value;
-import org.springframework.boot.autoconfigure.jdbc.DataSourceBuilder;
-import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.boot.test.SpringApplicationConfiguration;
-import org.springframework.context.annotation.*;
-import org.springframework.context.support.PropertySourcesPlaceholderConfigurer;
-import org.springframework.dao.annotation.PersistenceExceptionTranslationPostProcessor;
-import org.springframework.orm.hibernate4.HibernateTransactionManager;
-import org.springframework.orm.hibernate4.LocalSessionFactoryBean;
-import org.springframework.orm.hibernate4.LocalSessionFactoryBuilder;
-import org.springframework.orm.jpa.JpaTransactionManager;
-import org.springframework.orm.jpa.JpaVendorAdapter;
-import org.springframework.orm.jpa.LocalContainerEntityManagerFactoryBean;
-import org.springframework.orm.jpa.vendor.HibernateJpaVendorAdapter;
-import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.TestPropertySource;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
-import org.springframework.test.context.support.AnnotationConfigContextLoader;
 
-import javax.persistence.EntityManagerFactory;
-import javax.sql.DataSource;
-import javax.validation.constraints.NotNull;
-import java.util.*;
-
-import org.apache.solr.client.solrj.SolrServerException;
-import org.apache.solr.client.solrj.response.FacetField.Count;
-import org.springframework.transaction.PlatformTransactionManager;
-
+import java.util.List;
 import java.util.Map;
 
 
 @RunWith(SpringJUnit4ClassRunner.class)
-@SpringApplicationConfiguration(classes = {TestConfigSolr.class} )
+@SpringApplicationConfiguration(classes = {TestConfigSolr.class})
 @TestPropertySource("file:${user.home}/configfiles/${profile:dev}/application.properties")
 public class ExpressionServiceTest {
 
@@ -73,7 +46,6 @@ public class ExpressionServiceTest {
 			}
 
 		} catch (SolrServerException e) {
-			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
 	}
