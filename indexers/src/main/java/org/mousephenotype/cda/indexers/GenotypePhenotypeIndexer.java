bc61fff7c19cf83da579b64ba360f295acaa3f51
jmason <jmason@ebi.ac.uk>
2016-07-27 12:18:45 +0100

    remove sysout

--- indexers/src/main/java/org/mousephenotype/cda/indexers/GenotypePhenotypeIndexer.java
+++ indexers/src/main/java/org/mousephenotype/cda/indexers/GenotypePhenotypeIndexer.java
@@ -355,7 +355,6 @@ public class GenotypePhenotypeIndexer extends AbstractIndexer {
                     // Also check mappings up the tree, as a leaf term might not have a mapping, but the parents might.
                     Set<String> anatomyIdsForAncestors = new HashSet<>();
                     for (String mpAncestorId: mpOntologyService.getAncestorsDetail(mpId).getIds()){
-                    	System.out.println();
                     	if (mpOntologyService.getAnatomyMappings(mpAncestorId) != null){
                     		anatomyIdsForAncestors.addAll(mpOntologyService.getAnatomyMappings(mpAncestorId));
                     	}
