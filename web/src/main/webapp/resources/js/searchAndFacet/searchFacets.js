853cdface55c89bd0a6045c520d9f388fe37e2fa
ckchen <ckchen@ebi.ac.uk>
2016-09-14 13:25:58 +0100

    - facet filters now with word wrap when longer then 160px - product tab: mutation type becomes mutation where the content is a concatenation of mutation_type and allele_description - the alleles are ordered with CRISPR lines on top

--- web/src/main/webapp/resources/js/searchAndFacet/searchFacets.js
+++ web/src/main/webapp/resources/js/searchAndFacet/searchFacets.js
@@ -871,6 +871,7 @@
 				}
 			}
 
+			var defaultLen = 25;
 
 			// Subfacets: mutation_tyoe / allele category / allele feature
 			var oSubFacets1 = {'mutation_type_str':'Mutation Type', 'allele_category_str':'Allele Category', 'allele_features_str':'Allele Feature'};
