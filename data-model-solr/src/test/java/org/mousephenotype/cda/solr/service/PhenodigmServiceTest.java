5115ad8539cc458f47cc04ca4bb15237dde681bc
jmason <jmason@ebi.ac.uk>
2016-09-15 15:35:44 +0100

    add unique disease field to json (query from phenodigm)

---
+++ data-model-solr/src/test/java/org/mousephenotype/cda/solr/service/PhenodigmServiceTest.java
@@ -0,0 +1,33 @@
+package org.mousephenotype.cda.solr.service;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mousephenotype.cda.solr.TestConfigSolr;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.TestExecutionListeners;
+import org.springframework.test.context.TestPropertySource;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.test.context.support.DependencyInjectionTestExecutionListener;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@TestExecutionListeners( { DependencyInjectionTestExecutionListener.class })
+@ContextConfiguration(classes={TestConfigSolr.class})
+@TestPropertySource(locations = {"file:${user.home}/configfiles/${profile:dev}/application.properties"})
+public class PhenodigmServiceTest {
+
+    @Autowired
+    PhenodigmService phenodigmService;
+
+
+    @Test
+    public void getDiseaseAssociationCount() throws Exception {
+
+        final Integer diseaseAssociationCount = phenodigmService.getDiseaseAssociationCount();
+
+        System.out.println("Disease count: " + diseaseAssociationCount);
+        assert (diseaseAssociationCount > 0);
+
+    }
+
+}
