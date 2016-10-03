5b81b35b928d9e80a6274f948f792fd068dca343
jmason <jmason@ebi.ac.uk>
2016-10-03 15:37:57 +0100

    Do not require component scanning for this class

--- loads/src/main/java/org/mousephenotype/cda/loads/statistics/StatisticalDatasetGeneratorConfig.java
+++ loads/src/main/java/org/mousephenotype/cda/loads/statistics/StatisticalDatasetGeneratorConfig.java
@@ -21,14 +21,12 @@ import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.PropertySource;
 
 import javax.validation.constraints.NotNull;
 
 @Configuration
-@ComponentScan("org.mousephenotype.cda.loads.statistics")
 @PropertySource(value="file:${user.home}/configfiles/${profile:dev}/application.properties")
 @EnableAutoConfiguration
 public class StatisticalDatasetGeneratorConfig {
