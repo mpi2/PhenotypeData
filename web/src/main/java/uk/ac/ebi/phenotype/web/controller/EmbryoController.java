3a10db100177b39870aecc13d9c365876ae9d35b
Ilinca Tudose <ilinca.tudose@gmail.com>
2016-10-25 10:51:20 +0100

    Moved embryo page on landind path + redirect for old URL.

--- web/src/main/java/uk/ac/ebi/phenotype/web/controller/EmbryoController.java
+++ web/src/main/java/uk/ac/ebi/phenotype/web/controller/EmbryoController.java
@@ -1,16 +1,5 @@
 package uk.ac.ebi.phenotype.web.controller;
 
-import java.io.IOException;
-import java.net.URISyntaxException;
-import java.sql.SQLException;
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
-import java.util.Set;
-
-import javax.servlet.http.HttpServletRequest;
-
 import org.apache.solr.client.solrj.SolrServerException;
 import org.mousephenotype.cda.solr.service.GeneService;
 import org.mousephenotype.cda.solr.service.ObservationService;
@@ -20,9 +9,12 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
-
-import uk.ac.ebi.phenotype.chart.AnalyticsChartProvider;
 import uk.ac.ebi.phenotype.error.OntologyTermNotFoundException;
+
+import javax.servlet.http.HttpServletRequest;
+import java.io.IOException;
+import java.net.URISyntaxException;
+import java.sql.SQLException;
 
 /**
  * @author ilinca
@@ -41,18 +33,7 @@ public class EmbryoController {
 	public String loadPage(Model model, HttpServletRequest request, RedirectAttributes attributes)
 	throws OntologyTermNotFoundException, IOException, URISyntaxException, SolrServerException, SQLException {
 
-		AnalyticsChartProvider chartsProvider = new AnalyticsChartProvider();
-		List<String> resources = new ArrayList<>();
-		resources.add("IMPC");
-		Map<String, Set<String>> viabilityRes = os.getViabilityCategories(resources);
-
-		Map<String, Long> viabilityMap = os.getViabilityCategories(viabilityRes);
-		List<ObservationService.EmbryoTableRow> viabilityTable = os.consolidateZygosities(viabilityRes);
-		
-		model.addAttribute("viabilityChart", chartsProvider.getSlicedPieChart(new HashMap<String, Long> (), viabilityMap, "", "viabilityChart"));
-		model.addAttribute("viabilityTable", viabilityTable);
-		
-		return "embryo";
+		return "redirect:/landing/embryo";
 	}
 	
 	@RequestMapping(value = "/embryo/vignettes", method = RequestMethod.GET)
