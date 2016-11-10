881f58dcfdcb0ddb4c2c602dad9782d5d0aa9b89
jwgwarren <warren.jonathan@gmail.com>
2016-11-10 14:40:28 +0000

    changed image thumbnails by parameter to display on number of experimental images

--- web/src/main/java/uk/ac/ebi/phenotype/web/controller/GenesController.java
+++ web/src/main/java/uk/ac/ebi/phenotype/web/controller/GenesController.java
@@ -793,7 +793,7 @@ public class GenesController {
 	private void getImpcImages(String acc, Model model)
 	throws SolrServerException, IOException {
 
-		List<Group> groups = imageService.getPhenotypeAssociatedImages(acc, null, 1);
+		List<Group> groups = imageService.getPhenotypeAssociatedImages(acc, null, true, 1);
 		Map<String, String> paramToNumber=new HashMap<>();
 		for(Group group:groups){
 			if(!paramToNumber.containsKey(group.getGroupValue())){
