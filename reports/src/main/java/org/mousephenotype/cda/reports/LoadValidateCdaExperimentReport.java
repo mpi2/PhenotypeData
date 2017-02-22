767b694b3d5cc82376ac46f528d9abe6db6b77fc
mrelac <mrelac@ebi.ac.uk>
2017-02-22 16:00:44 +0000

    Refactored ExperimentDetail and added --count and --ignore arguments for random experimentId generation and column data value comparison ignore.

--- reports/src/main/java/org/mousephenotype/cda/reports/LoadValidateCdaExperimentReport.java
+++ reports/src/main/java/org/mousephenotype/cda/reports/LoadValidateCdaExperimentReport.java
@@ -16,6 +16,8 @@
 
 package org.mousephenotype.cda.reports;
 
+import joptsimple.OptionParser;
+import joptsimple.OptionSet;
 import org.apache.commons.lang3.ClassUtils;
 import org.apache.commons.lang3.StringUtils;
 import org.mousephenotype.cda.db.utilities.SqlUtils;
@@ -25,14 +27,17 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.core.env.PropertySource;
+import org.springframework.core.env.SimpleCommandLinePropertySource;
 import org.springframework.jdbc.core.namedparam.NamedParameterJdbcTemplate;
 import org.springframework.stereotype.Component;
 
 import javax.validation.constraints.NotNull;
 import java.beans.Introspector;
 import java.io.IOException;
-import java.util.Arrays;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 
 /**
  ** This report produces a file of cda/komp2 database differences between the supplied experimentIds.
@@ -46,6 +51,10 @@ public class LoadValidateCdaExperimentReport extends AbstractReport {
     private LoadValidateExperimentsQuery loadValidateExperimentsQuery;
     private SqlUtils                     sqlUtils = new SqlUtils();
 
+    private int         count            = 100;
+    private Set<String> experimentIdList = new HashSet<>();
+    private Set<String> ignoreList       = new HashSet<>();
+
     @Autowired
     @NotNull
     @Qualifier("jdbcCda1")
@@ -56,16 +65,59 @@ public class LoadValidateCdaExperimentReport extends AbstractReport {
     @Qualifier("jdbcCda2")
     private NamedParameterJdbcTemplate jdbc2;
 
+
+    public static final String COUNT_ARG = "count";
+    public static final String IGNORE_ARG = "ignore";
+    public static final String EXPERIMENT_ARG = "experiment";
+
     /****************************
-     * DATABASES: cda, komp2_base
+     * DATABASES: e.g. cda, komp2_base
      ****************************
     */
 
     @Override
     protected void initialise(String[] args) throws ReportException {
+        String message;
+
         super.initialise(args);
-        List<String> experimentIds = Arrays.asList(new String[] {"8852_1940", "8799_2575", "8964_1323"});
-        loadValidateExperimentsQuery = new LoadValidateExperimentsQuery(jdbc1, jdbc2, csvWriter, experimentIds);
+
+
+        PropertySource ps = new SimpleCommandLinePropertySource(args);
+
+        // Possible properties for this report: --count, --ignore
+        OptionParser parser = new OptionParser();
+
+        // parameter to indicate the count of experiment ids to generate and validate
+        parser.accepts("count").withRequiredArg().ofType(Integer.class);
+
+        // parameter to indicate the column alias(es) to ignore
+        parser.accepts("ignore").withRequiredArg().ofType(String.class);
+
+        // parameter to indicate any experimentIds to test
+        parser.accepts("experiment").withRequiredArg().ofType(String.class);
+
+        parser.allowsUnrecognizedOptions();         // Ignore options already processed at a higher level.
+
+        OptionSet options = parser.parse(args);
+
+        if (options.hasArgument("count")) {
+            Integer count = commonUtils.tryParseInt(options.valueOf("count"));
+            if (count == null) {
+                message = "Invalid count {}";
+                logger.error(message);
+                throw new ReportException(message);
+            }
+        }
+
+        if (options.hasArgument("ignore")) {
+            ignoreList.addAll((List<String>) options.valuesOf("ignore"));
+        }
+
+        if (options.hasArgument("experiment")) {
+            experimentIdList.addAll((List<String>) options.valuesOf("experiment"));
+        }
+
+        loadValidateExperimentsQuery = new LoadValidateExperimentsQuery(jdbc1, jdbc2, csvWriter, experimentIdList, count, ignoreList);
     }
 
     @Override
