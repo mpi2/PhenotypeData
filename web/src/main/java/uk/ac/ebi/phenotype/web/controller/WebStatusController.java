b4a0e585fd48557add50fcd3938ba3eb595a0881
jwgwarren <warren.jonathan@gmail.com>
2017-02-27 16:58:27 +0000

    made master webstatus same as live with synchronized method

--- web/src/main/java/uk/ac/ebi/phenotype/web/controller/WebStatusController.java
+++ web/src/main/java/uk/ac/ebi/phenotype/web/controller/WebStatusController.java
@@ -27,256 +27,247 @@ import java.util.concurrent.*;
 @Controller
 public class WebStatusController {
 
-	private final Logger logger = LoggerFactory.getLogger(WebStatusController.class);
+    private final Logger logger = LoggerFactory.getLogger(WebStatusController.class);
 
-	public static final Integer TIMEOUT_INTERVAL = 2;
+    public static final Integer TIMEOUT_INTERVAL = 2;
 
-	@Autowired
-	ObservationService observationService;
+    @Autowired
+    ObservationService observationService;
 
-	@Autowired
-	private PostQcService postqcService;
+    @Autowired
+    private PostQcService postqcService;
 
-	@Autowired
-	private StatisticalResultService srService;
+    @Autowired
+    private StatisticalResultService srService;
 
-	@Autowired
-	private PreQcService preqcService;
+    @Autowired
+    private PreQcService preqcService;
 
-	@Autowired
-	private AlleleService alleleService;
+    @Autowired
+    private AlleleService alleleService;
 
-	@Autowired
-	private ImagesSolrJ sangerImages;
+    @Autowired
+    private ImagesSolrJ sangerImages;
 
-	@Autowired
-	ImageService impcImageService;
+    @Autowired
+    ImageService impcImageService;
 
-	@Autowired
-	MpService mpService;
+    @Autowired
+    MpService mpService;
 
-	@Autowired
-	AnatomyService anatomyService;
+    @Autowired
+    AnatomyService anatomyService;
 
-	@Autowired
-	ImpressService pipelineService;
+    @Autowired
+    ImpressService pipelineService;
 
-	@Autowired
-	private GeneService geneService;
+    @Autowired
+    private GeneService geneService;
 
-	@Autowired
-	DiseaseService diseaseService;
+    @Autowired
+    DiseaseService diseaseService;
 
-	@Autowired
-	AutoSuggestService autoSuggestService;
+    @Autowired
+    AutoSuggestService autoSuggestService;
 
-	@Autowired
-	private PhenotypePipelineDAO ppDAO;
+    @Autowired
+    private PhenotypePipelineDAO ppDAO;
 
-	@Autowired
-	PhenodigmService phenodigmService;
+    @Autowired
+    PhenodigmService phenodigmService;
 
-	List<WebStatus> webStatusObjects;
+    List<WebStatus> webStatusObjects;
 
-	// imits solr services
+    // imits solr services
 
-	@Autowired
-	Allele2Service allele2;
+    @Autowired
+    Allele2Service allele2;
 
-	@Autowired
-	ProductService productService;
+    @Autowired
+    ProductService productService;
 
-	List<WebStatus> nonEssentialWebStatusObjects;
-	private Model savedModel = null;
-	private Integer cacheHit = 0;
-	private Integer cacheMiss = 0;
+    List<WebStatus> nonEssentialWebStatusObjects;
+    private Model savedModel = null;
+    private Integer cacheHit = 0;
+    private Integer cacheMiss = 0;
 
-	@PostConstruct
-	public void initialise() {
+    @PostConstruct
+    public void initialise() {
 
-		// cores we need to test are at least this set:
-		// experiment,genotype-phenotype,statistical-result,preqc,allele,images,impc_images,mp,ma,pipeline,gene,disease,autosuggest
-		// System.out.println("calling webStatus initialisation method");
-		webStatusObjects = new ArrayList<>();
-		webStatusObjects.add(observationService);
-		webStatusObjects.add(postqcService);
-		webStatusObjects.add(srService);
-		webStatusObjects.add(preqcService);
-		webStatusObjects.add(alleleService);
-		webStatusObjects.add(sangerImages);
-		webStatusObjects.add(impcImageService);
-		webStatusObjects.add(mpService);
-		webStatusObjects.add(anatomyService);
-		webStatusObjects.add(pipelineService);
-		webStatusObjects.add(geneService);
-		webStatusObjects.add(diseaseService);
-		webStatusObjects.add(autoSuggestService);
-		webStatusObjects.add(ppDAO);
-		webStatusObjects.add(phenodigmService);
-				
-		nonEssentialWebStatusObjects = new ArrayList<>();
-		nonEssentialWebStatusObjects.add(allele2);
-		nonEssentialWebStatusObjects.add(productService);
-	}
+        // cores we need to test are at least this set:
+        // experiment,genotype-phenotype,statistical-result,preqc,allele,images,impc_images,mp,ma,pipeline,gene,disease,autosuggest
+        // System.out.println("calling webStatus initialisation method");
+        webStatusObjects = new ArrayList<>();
+        webStatusObjects.add(observationService);
+        webStatusObjects.add(postqcService);
+        webStatusObjects.add(srService);
+        webStatusObjects.add(preqcService);
+        webStatusObjects.add(alleleService);
+        webStatusObjects.add(sangerImages);
+        webStatusObjects.add(impcImageService);
+        webStatusObjects.add(mpService);
+        webStatusObjects.add(anatomyService);
+        webStatusObjects.add(pipelineService);
+        webStatusObjects.add(geneService);
+        webStatusObjects.add(diseaseService);
+        webStatusObjects.add(autoSuggestService);
+        webStatusObjects.add(ppDAO);
+        webStatusObjects.add(phenodigmService);
 
-	@RequestMapping("/clearCache")
-	public ModelAndView clearCache() {
-		synchronized (this) {
-			savedModel = null;
-		}
-		return new ModelAndView("redirect:webstatus");
+        nonEssentialWebStatusObjects = new ArrayList<>();
+        nonEssentialWebStatusObjects.add(allele2);
+        nonEssentialWebStatusObjects.add(productService);
+    }
+
+    @RequestMapping("/clearCache")
+    public synchronized ModelAndView clearCache() {
+        savedModel = null;
+        return new ModelAndView("redirect:webstatus");
 
 
-	}
+    }
 
-	@RequestMapping("/webstatus")
-	public String webStatus(Model model, HttpServletResponse response) {
+    @RequestMapping("/webstatus")
+    public synchronized String webStatus(Model model, HttpServletResponse response) {
 
-		if (savedModel != null && savedModel.containsAttribute("webStatusModels") && Math.random() < 0.95) {
-			synchronized (this) {
-				cacheHit += 1;
-			}
+        if (savedModel != null && savedModel.containsAttribute("webStatusModels") && Math.random() < 0.95) {
+            cacheHit += 1;
 
-			model.addAllAttributes(savedModel.asMap());
+            model.addAllAttributes(savedModel.asMap());
 
-			model.addAttribute("hits", cacheHit);
-			model.addAttribute("misses", cacheMiss);
-			model.addAttribute("ratio", cacheHit+cacheMiss>0 ? ((float)cacheHit / (cacheHit+cacheMiss)): "Not available yet");
+            model.addAttribute("hits", cacheHit);
+            model.addAttribute("misses", cacheMiss);
+            model.addAttribute("ratio", cacheHit+cacheMiss>0 ? ((float)cacheHit / (cacheHit+cacheMiss)): "Not available yet");
 
-			return "webStatus";
-		}
+            return "webStatus";
+        }
 
-		logger.info("Updating webstatus model values");
-		synchronized (this) {
-			cacheMiss += 1;
-		}
+        logger.info("Updating webstatus model values");
+        cacheMiss += 1;
 
-		ExecutorService executor = Executors.newCachedThreadPool();
+        ExecutorService executor = Executors.newCachedThreadPool();
 
-		boolean ok = true;
+        boolean ok = true;
 
-		// check our core solr instances are returning via the services
-		List<WebStatusModel> webStatusModels = new ArrayList<>();
-		for (WebStatus status : webStatusObjects) {
+        // check our core solr instances are returning via the services
+        List<WebStatusModel> webStatusModels = new ArrayList<>();
+        for (WebStatus status : webStatusObjects) {
 
-			Future<Long> future = null;
+            Future<Long> future = null;
 
-			String name = status.getServiceName();
-			long number = 0;
-			try {
+            String name = status.getServiceName();
+            long number = 0;
+            try {
 
-				// This block causes the method reference getWebStatus to be submitted to the executor
-				// And then "get"ted from the future.  If the request is not complete in 2 seconds (more than enough time)
-				// then timeout and throw an exception
-				Callable<Long> task = status::getWebStatus;
-				future = executor.submit(task);
-				number = future.get(TIMEOUT_INTERVAL, TimeUnit.SECONDS);
+                // This block causes the method reference getWebStatus to be submitted to the executor
+                // And then "get"ted from the future.  If the request is not complete in 2 seconds (more than enough time)
+                // then timeout and throw an exception
+                Callable<Long> task = status::getWebStatus;
+                future = executor.submit(task);
+                number = future.get(TIMEOUT_INTERVAL, TimeUnit.SECONDS);
 
-			} catch (Exception e) {
+            } catch (Exception e) {
 
-				// Cancel the ongoing call
-				if (future!=null) {future.cancel(true);}
+                // Cancel the ongoing call
+                if (future!=null) {future.cancel(true);}
 
-				ok = false;
+                ok = false;
 
-				// Do not cache the model if it is not ok.  This will force the status check
-				// to occur on subsequent page loads.
-				synchronized (this) {
-					savedModel = null;
-				}
+                // Do not cache the model if it is not ok.  This will force the status check
+                // to occur on subsequent page loads.
+                savedModel = null;
 
-				logger.error("Essential service {} is not available", name);
-				e.printStackTrace();
-				break;
-			}
+                logger.error("Essential service {} is not available", name);
+                e.printStackTrace();
+                break;
+            }
 
-			WebStatusModel wModel = new WebStatusModel(name, number);
-			webStatusModels.add(wModel);
-		}
+            WebStatusModel wModel = new WebStatusModel(name, number);
+            webStatusModels.add(wModel);
+        }
 
-		model.addAttribute("webStatusModels", webStatusModels);
+        model.addAttribute("webStatusModels", webStatusModels);
 
-		// check the imits services
-		List<WebStatusModel> nonEssentialWebStatusModels = new ArrayList<>();
-		boolean nonEssentialOk=true;
-		for (WebStatus status : nonEssentialWebStatusObjects) {
+        // check the imits services
+        List<WebStatusModel> nonEssentialWebStatusModels = new ArrayList<>();
+        boolean nonEssentialOk=true;
+        for (WebStatus status : nonEssentialWebStatusObjects) {
 
-			Future<Long> future = null;
-			String name = status.getServiceName();
-			long number = 0;
+            Future<Long> future = null;
+            String name = status.getServiceName();
+            long number = 0;
 
-			try {
-				// number = status.getWebStatus();
+            try {
+                // number = status.getWebStatus();
 
-				// This block causes the method reference getWebStatus to be submitted to the executor
-				// And then "get"ted from the future.  If the request is not complete in 2 seconds (more than enough time)
-				// then timeout and throw an exception
-				Callable<Long> task = status::getWebStatus;
-				future = executor.submit(task);
-				number = future.get(TIMEOUT_INTERVAL, TimeUnit.SECONDS);
+                // This block causes the method reference getWebStatus to be submitted to the executor
+                // And then "get"ted from the future.  If the request is not complete in 2 seconds (more than enough time)
+                // then timeout and throw an exception
+                Callable<Long> task = status::getWebStatus;
+                future = executor.submit(task);
+                number = future.get(TIMEOUT_INTERVAL, TimeUnit.SECONDS);
 
-			} catch (Exception e) {
+            } catch (Exception e) {
 
-				// Cancel the ongoing call
-				if (future!=null) {future.cancel(true);}
+                // Cancel the ongoing call
+                if (future!=null) {future.cancel(true);}
 
-				// Do not change the website status for an unavailable non-critical resource
-				nonEssentialOk=false;
-				logger.error("Non essential service {} is not available", name);
-				e.printStackTrace();
-			}
-			WebStatusModel wModel = new WebStatusModel(name, number);
-			nonEssentialWebStatusModels.add(wModel);
-		}
+                // Do not change the website status for an unavailable non-critical resource
+                nonEssentialOk=false;
+                logger.error("Non essential service {} is not available", name);
+                e.printStackTrace();
+            }
+            WebStatusModel wModel = new WebStatusModel(name, number);
+            nonEssentialWebStatusModels.add(wModel);
+        }
 
-		model.addAttribute("nonEssentialWebStatusModels", nonEssentialWebStatusModels);
-		if (ok) {
-			response.setStatus(HttpServletResponse.SC_OK);
-		} else {
-			response.setStatus(HttpServletResponse.SC_INTERNAL_SERVER_ERROR);
-		}
-		model.addAttribute("ok", ok);
-		model.addAttribute("nonEssentialOk",nonEssentialOk);
-		model.addAttribute("hits", cacheHit);
-		model.addAttribute("misses", cacheMiss);
-		model.addAttribute("ratio", cacheHit+cacheMiss>0 ? ((float)cacheHit / (cacheHit+cacheMiss)): "Not available yet");
+        model.addAttribute("nonEssentialWebStatusModels", nonEssentialWebStatusModels);
+        if (ok) {
+            response.setStatus(HttpServletResponse.SC_OK);
+        } else {
+            response.setStatus(HttpServletResponse.SC_INTERNAL_SERVER_ERROR);
+        }
+        model.addAttribute("ok", ok);
+        model.addAttribute("nonEssentialOk",nonEssentialOk);
+        model.addAttribute("hits", cacheHit);
+        model.addAttribute("misses", cacheMiss);
+        model.addAttribute("ratio", cacheHit+cacheMiss>0 ? ((float)cacheHit / (cacheHit+cacheMiss)): "Not available yet");
 
-		// If everything is ok, cache the model to be used later
-		synchronized (this) {
-			if (ok) {
-				savedModel = model;
-			}
-		}
+        // If everything is ok, cache the model to be used later
+        if (ok) {
+            savedModel = model;
+        }
 
-		return "webStatus";
-	}
 
-	public class WebStatusModel {
+        return "webStatus";
+    }
 
-		public String name;
-		public long number;
+    public class WebStatusModel {
 
-		public WebStatusModel(String name, long number) {
-			this.name = name;
-			this.number = number;
-		}
+        public String name;
+        public long number;
 
-		public String getName() {
-			return name;
-		}
+        public WebStatusModel(String name, long number) {
+            this.name = name;
+            this.number = number;
+        }
 
-		public void setName(String name) {
-			this.name = name;
-		}
+        public String getName() {
+            return name;
+        }
 
-		public long getNumber() {
-			return number;
-		}
+        public void setName(String name) {
+            this.name = name;
+        }
 
-		public void setNumber(long number) {
-			this.number = number;
-		}
+        public long getNumber() {
+            return number;
+        }
 
-	}
+        public void setNumber(long number) {
+            this.number = number;
+        }
+
+    }
 
 }
