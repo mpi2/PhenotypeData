1c931b9860ef4588218906c30d5e33538a0c0edf
Ilinca Tudose <ilinca.tudose@gmail.com>
2017-03-01 17:31:13 +0000

    Started Class config.

--- web/src/main/java/uk/ac/ebi/phenotype/web/util/DeploymentInterceptor.java
+++ web/src/main/java/uk/ac/ebi/phenotype/web/util/DeploymentInterceptor.java
@@ -15,20 +15,20 @@
  *******************************************************************************/
 package uk.ac.ebi.phenotype.web.util;
 
-import java.text.SimpleDateFormat;
-import java.util.Calendar;
-import java.util.HashMap;
-import java.util.Map;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+import org.springframework.web.servlet.handler.HandlerInterceptorAdapter;
 
 import javax.annotation.Resource;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.servlet.handler.HandlerInterceptorAdapter;
-
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.HashMap;
+import java.util.Map;
+@Component
 public class DeploymentInterceptor extends HandlerInterceptorAdapter {
 
     private final Logger log = LoggerFactory.getLogger(this.getClass().getCanonicalName());
