4c0db956faeb340732940c76c73c977ace5e880f
ckchen <ckchen@ebi.ac.uk>
2017-04-10 13:07:45 +0100

    added new repos files

---
+++ loads/src/main/java/org/mousephenotype/cda/loads/graph/Neo4jConfig.java
@@ -0,0 +1,57 @@
+package org.mousephenotype.cda.loads.graph;
+
+import org.neo4j.ogm.session.SessionFactory;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Import;
+import org.springframework.context.annotation.PropertySource;
+import org.springframework.data.neo4j.repository.config.EnableNeo4jRepositories;
+import org.springframework.data.neo4j.transaction.Neo4jTransactionManager;
+
+import javax.validation.constraints.NotNull;
+
+/**
+ * Created by jmason on 18/03/2017.
+ */
+@Configuration
+@EnableNeo4jRepositories(basePackages = "org.mousephenotype.cda.neo4j.repository")
+@PropertySource("file:${user.home}/configfiles/${profile:dev}/application.properties")
+public class Neo4jConfig {
+
+    @NotNull
+    @Value("${neo4jDbPath}")
+    private String neo4jDbPath;
+
+    @NotNull
+    @Value("${owlpath}")
+    protected String owlpath;
+
+    @Bean
+    public org.neo4j.ogm.config.Configuration getConfiguration() {
+        org.neo4j.ogm.config.Configuration config = new org.neo4j.ogm.config.Configuration();
+
+        // To persist the database, uncomment this section
+        //String pathToDb = Paths.get(".").toAbsolutePath().normalize().toString() + "/target//Users/ckchen/Documents/Neo4j/impc.neo4";
+        String pathToDb = neo4jDbPath;
+        config
+                .driverConfiguration()
+                .setDriverClassName("org.neo4j.ogm.drivers.embedded.driver.EmbeddedDriver")
+                .setURI("file://" + pathToDb);
+
+        System.out.println(config);
+
+        return config;
+    }
+
+    @Bean
+    public SessionFactory sessionFactory() {
+        return new SessionFactory(getConfiguration(), "org.mousephenotype.cda.neo4j");  // both entity and repository
+    }
+
+    @Bean
+    public Neo4jTransactionManager transactionManager() {
+        return new Neo4jTransactionManager(sessionFactory());
+    }
+
+}
