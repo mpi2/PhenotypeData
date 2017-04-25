e52e4a9883dea1329fbfea180d4e5b03da062e21
ckchen <ckchen@ebi.ac.uk>
2017-04-25 15:47:40 +0100

    added new stats result entity and repository. Updated loader for stats results

--- data-model-db/src/main/java/org/mousephenotype/cda/neo4j/entity/StatisticalResult.java
+++ data-model-db/src/main/java/org/mousephenotype/cda/neo4j/entity/StatisticalResult.java
@@ -4,6 +4,7 @@ import org.neo4j.ogm.annotation.GraphId;
 import org.neo4j.ogm.annotation.NodeEntity;
 import org.neo4j.ogm.annotation.Relationship;
 
+import java.util.List;
 import java.util.Set;
 
 /**
@@ -15,100 +16,14 @@ public class StatisticalResult {
     @GraphId
     Long id;
 
-    private String ResourceName;
-    private String ResourceFullname;
-    private int resourceId;
-    private String projectName;
+    private Integer dbId;
+    private List<String> phenotypeSex; // male/female or both
     private String phenotypingCenter;
     private String colonyId;
-    private String strainName;
-    private String strainAccessionId;
-    private String sex;
     private String zygosity;
-    private String controlSelectionMethod;
-    private String dependentVariable;
-    private String metadataGroup;
-    private String dataFrame;
-    private int externalDbId;
-    private int projectId;
-
-    // Information; about the raw data
-    private int controlBiologicalModelId;
-    private int mutantBiologicalModelId;
-    private int maleControlCount;
-    private int maleMutantCount;
-    private int femaleControlCount;
-    private int femaleMutantCount;
-    private double maleControlMean;
-    private double maleMutantMean;
-    private double femaleControlMean;
-    private double femaleMutantMean;
-
-    //Information; about the calculation
-    private String workflow;
-    private String statisticalMethod;
-    private String status;
-    private String additionalInformation;
-    private String rawOutput;
-    private double pValue;
-    private double effectSize;
-
-    //Reference; Range Plus statistics details
-    private double genotypePvalueLowVsNormalHigh;
-    private double genotypePvalueLowNormalVsHigh;
-    private double genotypeEffectSizeLowVsNormalHigh;
-    private double genotypeEffectSizeLowNormalVsHigh;
-    private double femalePvalueLowVsNormalHigh;
-    private double femalePvalueLowNormalVsHigh;
-    private double femaleEffectSizeLowVsNormalHigh;
-    private double femaleEffectSizeLowNormalVsHigh;
-    private double malePvalueLowVsNormalHigh;
-    private double malePvalueLowNormalVsHigh;
-    private double maleEffectSizeLowVsNormalHigh;
-    private double maleEffectSizeLowNormalVsHigh;
-
-    //Fisher; exact statistics details
-    private String categories;
-    private double categoricalPValue;
-    private double categoricalEffectSize;
-
-    //MM;/LM statistic details
-    private boolean batchSignificant;
-    private boolean varianceSignificant;
-    private double nullTestPValue;
-    private double genotypeEffectPValue;
-    private double genotypeEffectStderrEstimate;
-    private double genotypeEffectParameterEstimate;
-    private String malePercentageChange;
-    private String femalePercentageChange;
-    private double sexEffectPValue;
-    private double sexEffectStderrEstimate;
-    private double sexEffectParameterEstimate;
-    private double weightEffectPValue;
-    private double weightEffectStderrEstimate;
-    private double weightEffectParameterEstimate;
-    private String group_1Genotype;
-    private double group_1ResidualsNormalityTest;
-    private String group_2Genotype;
-    private double group_2ResidualsNormalityTest;
-    private double blupsTest;
-    private double rotatedResidualsTest;
-    private double interceptEstimate;
-    private double interceptEstimateStderrEstimate;
-    private boolean interactionSignificant;
-    private double interactionEffectPValue;
-    private double femaleKoEffectPValue;
-    private double femaleKoEffectStderrEstimate;
-    private double femaleKoParameterEstimate;
-    private double maleKoEffectPValue;
-    private double maleKoEffectStderrEstimate;
-    private double maleKoParameterEstimate;
-    private String classificationTag;
-    private String phenotypeSex;
-    private String lifeStageAcc;
-    private String lifeStageName;
+    private Double pValue;
+    private Double effectSize;
     private boolean significant;
-
 
     @Relationship(type="GENE", direction=Relationship.OUTGOING)
     private Gene gene;
@@ -117,19 +32,7 @@ public class StatisticalResult {
     private Allele allele;
 
     @Relationship(type="MP", direction=Relationship.OUTGOING)
-    private Mp mp;
-
-    @Relationship(type="MALE_MP", direction=Relationship.OUTGOING)
-    private Mp maleMp;
-
-    @Relationship(type="FEMALE_MP", direction=Relationship.OUTGOING)
-    private Mp femaleMp;
-
-    @Relationship(type="MP_OPTIONS", direction=Relationship.OUTGOING)
-    private Set<Mp> mpOptions;
-
-    @Relationship(type="PIPELINE", direction=Relationship.OUTGOING)
-    private Pipeline pipeline;
+    private Set<Mp> mps;
 
     @Relationship(type="PROCEDURE", direction=Relationship.OUTGOING)
     private Procedure procedure;
@@ -137,4 +40,135 @@ public class StatisticalResult {
     @Relationship(type="PARAMETER", direction=Relationship.OUTGOING)
     private Parameter parameter;
 
+    public Long getId() {
+        return id;
+    }
+
+    public Integer getDbId() {
+        return dbId;
+    }
+
+    public void setDbId(Integer dbId) {
+        this.dbId = dbId;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public List<String> getPhenotypeSex() {
+        return phenotypeSex;
+    }
+
+    public void setPhenotypeSex(List<String> phenotypeSex) {
+        this.phenotypeSex = phenotypeSex;
+    }
+
+    public String getPhenotypingCenter() {
+        return phenotypingCenter;
+    }
+
+    public void setPhenotypingCenter(String phenotypingCenter) {
+        this.phenotypingCenter = phenotypingCenter;
+    }
+
+    public String getColonyId() {
+        return colonyId;
+    }
+
+    public void setColonyId(String colonyId) {
+        this.colonyId = colonyId;
+    }
+
+    public String getZygosity() {
+        return zygosity;
+    }
+
+    public void setZygosity(String zygosity) {
+        this.zygosity = zygosity;
+    }
+
+    public Double getpValue() {
+        return pValue;
+    }
+
+    public void setpValue(Double pValue) {
+        this.pValue = pValue;
+    }
+
+    public Double getEffectSize() {
+        return effectSize;
+    }
+
+    public void setEffectSize(Double effectSize) {
+        this.effectSize = effectSize;
+    }
+
+    public boolean isSignificant() {
+        return significant;
+    }
+
+    public void setSignificant(boolean significant) {
+        this.significant = significant;
+    }
+
+    public Gene getGene() {
+        return gene;
+    }
+
+    public void setGene(Gene gene) {
+        this.gene = gene;
+    }
+
+    public Allele getAllele() {
+        return allele;
+    }
+
+    public void setAllele(Allele allele) {
+        this.allele = allele;
+    }
+
+    public Set<Mp> getMps() {
+        return mps;
+    }
+
+    public void setMps(Set<Mp> mps) {
+        this.mps = mps;
+    }
+
+    public Procedure getProcedure() {
+        return procedure;
+    }
+
+    public void setProcedure(Procedure procedure) {
+        this.procedure = procedure;
+    }
+
+    public Parameter getParameter() {
+        return parameter;
+    }
+
+    public void setParameter(Parameter parameter) {
+        this.parameter = parameter;
+    }
+
+    @Override
+    public String toString() {
+        return "StatisticalResult{" +
+                "id=" + id +
+                ", dbId=" + dbId +
+                ", phenotypeSex=" + phenotypeSex +
+                ", phenotypingCenter='" + phenotypingCenter + '\'' +
+                ", colonyId='" + colonyId + '\'' +
+                ", zygosity='" + zygosity + '\'' +
+                ", pValue=" + pValue +
+                ", effectSize=" + effectSize +
+                ", significant=" + significant +
+                ", gene=" + gene +
+                ", allele=" + allele +
+                ", mps=" + mps +
+                ", procedure=" + procedure +
+                ", parameter=" + parameter +
+                '}';
+    }
 }
