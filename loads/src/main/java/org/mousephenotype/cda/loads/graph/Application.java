c67631c9ab428a0e9d5bd9c58cfcf992c5516ab1
ckchen <ckchen@ebi.ac.uk>
2017-04-26 19:31:18 +0100

    sout to test where the bug might be

--- loads/src/main/java/org/mousephenotype/cda/loads/graph/Application.java
+++ loads/src/main/java/org/mousephenotype/cda/loads/graph/Application.java
@@ -22,7 +22,8 @@ import org.springframework.boot.builder.SpringApplicationBuilder;
 public class Application {
 
     public static void main(String[] args) throws Exception {
-        //new SpringApplicationBuilder(Application.class).web(false).run(args);
+        System.out.println("About to start application");
+
         SpringApplication.run(Application.class, args);
     }
 }
