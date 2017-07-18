617f4c69eb17adc297b4f01d4499d2dca2a2412e
ckchen <ckchen@ebi.ac.uk>
2017-07-18 12:04:24 +0100

    - expanded PhenotypeFormObject

--- web/src/main/java/uk/ac/ebi/phenotype/service/PhenotypeFormObject.java
+++ web/src/main/java/uk/ac/ebi/phenotype/service/PhenotypeFormObject.java
@@ -4,10 +4,27 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class PhenotypeFormObject {
-	
+
+	// inputs and checkboxes
 	private Logical logical1;
 	private Logical logical2;
 	private List<PhenotypeFormEntryRow> phenotypeFormRows=new ArrayList<>();
+	private Boolean excludeNestedPhenotype;
+	private String parameterName;
+	private Boolean significantPvaluesOnly;
+
+	//customed output columns
+	private Boolean showMpTerm;
+	private Boolean showMpTermSynonym;
+	private Boolean showMpId;
+	private Boolean showMpDefinition;
+
+	private Boolean showTopLevelMpTerm;
+	private Boolean showTopLevelMpId;
+
+	private Boolean showParameterName;
+	private Boolean showPvalue;
+
 
 	public Logical getLogical1() {
 		return logical1;
@@ -15,12 +32,14 @@ public class PhenotypeFormObject {
 	public void setLogical1(Logical logical1) {
 		this.logical1 = logical1;
 	}
+
 	public Logical getLogical2() {
 		return logical2;
 	}
 	public void setLogical2(Logical logical2) {
 		this.logical2 = logical2;
 	}
+
 	public List<PhenotypeFormEntryRow> getPhenotypeFormRows() {
 		return phenotypeFormRows;
 	}
@@ -33,5 +52,92 @@ public class PhenotypeFormObject {
 			System.err.println("Phenotype form rows exceeds 3 - max is currently set to 3");
 		}
 	}
-	
+
+	public Boolean getExcludeNestedPhenotype() {
+		return excludeNestedPhenotype;
+	}
+
+	public void setExcludeNestedPhenotype(Boolean excludeNestedPhenotype) {
+		this.excludeNestedPhenotype = excludeNestedPhenotype;
+	}
+
+	public String getParameterName() {
+		return parameterName;
+	}
+
+	public void setParameterName(String parameterName) {
+		this.parameterName = parameterName;
+	}
+
+	public Boolean getSignificantPvaluesOnly() {
+		return significantPvaluesOnly;
+	}
+
+	public void setSignificantPvaluesOnly(Boolean significantPvaluesOnly) {
+		this.significantPvaluesOnly = significantPvaluesOnly;
+	}
+
+	public Boolean getShowMpTerm() {
+		return showMpTerm;
+	}
+
+	public void setShowMpTerm(Boolean showMpTerm) {
+		this.showMpTerm = showMpTerm;
+	}
+
+	public Boolean getShowMpTermSynonym() {
+		return showMpTermSynonym;
+	}
+
+	public void setShowMpTermSynonym(Boolean showMpTermSynonym) {
+		this.showMpTermSynonym = showMpTermSynonym;
+	}
+
+	public Boolean getShowMpId() {
+		return showMpId;
+	}
+
+	public void setShowMpId(Boolean showMpId) {
+		this.showMpId = showMpId;
+	}
+
+	public Boolean getShowMpDefinition() {
+		return showMpDefinition;
+	}
+
+	public void setShowMpDefinition(Boolean showMpDefinition) {
+		this.showMpDefinition = showMpDefinition;
+	}
+
+	public Boolean getShowTopLevelMpTerm() {
+		return showTopLevelMpTerm;
+	}
+
+	public void setShowTopLevelMpTerm(Boolean showTopLevelMpTerm) {
+		this.showTopLevelMpTerm = showTopLevelMpTerm;
+	}
+
+	public Boolean getShowTopLevelMpId() {
+		return showTopLevelMpId;
+	}
+
+	public void setShowTopLevelMpId(Boolean showTopLevelMpId) {
+		this.showTopLevelMpId = showTopLevelMpId;
+	}
+
+	public Boolean getShowParameterName() {
+		return showParameterName;
+	}
+
+	public void setShowParameterName(Boolean showParameterName) {
+		this.showParameterName = showParameterName;
+	}
+
+	public Boolean getShowPvalue() {
+		return showPvalue;
+	}
+
+	public void setShowPvalue(Boolean showPvalue) {
+		this.showPvalue = showPvalue;
+	}
 }
