240f71aeb60d1c5ed235e91d40d7fe3385f183f6
ckchen <ckchen@ebi.ac.uk>
2017-08-01 17:11:59 +0100

    added tests for AdvancedSearch

--- web/src/test/java/uk/ac/ebi/phenotype/service/AdvancedSearchServiceTest.java
+++ web/src/test/java/uk/ac/ebi/phenotype/service/AdvancedSearchServiceTest.java
@@ -5,10 +5,15 @@ import java.lang.reflect.InvocationTargetException;
 import java.util.*;
 
 import org.apache.solr.client.solrj.SolrClient;
+import org.apache.solr.client.solrj.SolrQuery;
 import org.apache.solr.client.solrj.SolrServerException;
+import org.apache.solr.client.solrj.response.QueryResponse;
+import org.apache.solr.common.SolrDocument;
+import org.apache.solr.common.SolrDocumentList;
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.mousephenotype.cda.neo4j.entity.Gene;
 import org.mousephenotype.cda.solr.service.PostQcService;
 import org.neo4j.ogm.session.Session;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -20,6 +25,8 @@ import org.springframework.test.context.junit4.SpringRunner;
 import org.neo4j.ogm.model.Result;
 import uk.ac.ebi.phenotype.web.Neo4jConfig;
 import uk.ac.ebi.phenotype.web.TestAdvancedSearchConfig;
+
+import static org.junit.Assert.assertTrue;
 
 @RunWith(SpringRunner.class)
 @ContextConfiguration(classes={TestAdvancedSearchConfig.class, Neo4jConfig.class})
@@ -37,53 +44,258 @@ public class AdvancedSearchServiceTest {
     @Qualifier("autosuggestCore")
     private SolrClient autosuggestCore;
 
+    @Autowired
+    @Qualifier("allele2Core")
+    private SolrClient allele2Core;
+
+    @Autowired
+    @Qualifier("statisticalResultCore")
+    private SolrClient statisticalResultCore;
+
+    @Autowired
+    @Qualifier("genotypePhenotypeCore")
+    private SolrClient genotypePhenotypeCore;
+
     private AdvancedSearchService advancedSearchService;
+
     @Before
-    public void setUp(){
+    public void setUp() {
         advancedSearchService = new AdvancedSearchService(gpService, autosuggestCore, neo4jSession);
     }
 
-    @Test
-    public void testGenesByMpTerm() throws InvocationTargetException, NoSuchMethodException, IllegalAccessException, SolrServerException, IOException, InterruptedException {
+
+    //@Test
+    public void testGenesByMpTermFromNeo4jAndSolr() throws InvocationTargetException, NoSuchMethodException, IllegalAccessException, SolrServerException, IOException, InterruptedException {
 
         AdvancedSearchPhenotypeForm mpForm = new AdvancedSearchPhenotypeForm();
 
+        String mpTerm = "abnormal glucose homeostasis";
+        //String mpTerm = "abnormal retina morphology";
+
         //AdvancedSearchMpRow mpRow = new AdvancedSearchMpRow("abnormal glucose homeostasis", 0.00001, 0.0001);
-        AdvancedSearchMpRow mpRow = new AdvancedSearchMpRow("abnormal retina morphology", null, null);
+        AdvancedSearchMpRow mpRow = new AdvancedSearchMpRow(mpTerm, null, null);
         mpForm.addPhenotypeRows(mpRow);
+        mpForm.setSignificantPvaluesOnly(true);
 //        mpForm.setHasOutputColumn(true);
 //        mpForm.setShowMpTerm(true);
-        System.out.println("MP FORM: " + mpForm.toString());
+        //System.out.println("MP FORM: " + mpForm.toString());
 
         AdvancedSearchGeneForm geneForm = new AdvancedSearchGeneForm();
         //geneForm.setChrIds(Arrays.asList("1"));
-        System.out.println("GENE FORM: " + geneForm.toString());
+        //System.out.println("GENE FORM: " + geneForm.toString());
 
         AdvancedSearchDiseaseForm diseaseForm = new AdvancedSearchDiseaseForm();
-        System.out.println("DISEASE FORM: " + diseaseForm.toString());
+        //System.out.println("DISEASE FORM: " + diseaseForm.toString());
 
         String fileType = null; // only needed for export
 
         List<Object> objects = advancedSearchService.fetchGraphDataAdvSrchResult(mpForm, geneForm, diseaseForm, fileType);
         Result result = (Result) objects.get(0);
 
+        Set<String> n4jSymbols = new HashSet<>();
+        int genesFoundNeo4j = 0;
         // do something with the result
-        for (Map<String,Object> row : result) {
-        	//System.out.println("row="+row);
+        for (Map<String, Object> row : result) {
+            //System.out.println("row="+row);
             for (Map.Entry<String, Object> entry : row.entrySet()) {
-                if (entry.getValue() != null && ! entry.getValue().toString().startsWith("[Ljava.lang.Object")) {
+                if (entry.getValue() != null && !entry.getValue().toString().startsWith("[Ljava.lang.Object")) {
                     List<Object> objs = (List<Object>) entry.getValue();
 
-                    System.out.println(entry.getKey() + " -- " + objs.size());
-                    //for(Object obj: objs){
-                    //System.out.println(obj);
-                    //}
+                    //System.out.println(entry.getKey() + " -- " + objs.size());
+                    if (entry.getKey().equals("collect(distinct g)")) {
+                        genesFoundNeo4j = objs.size();
+
+                        for (Object obj : objs) {
+                            //System.out.println(obj);
+                            Gene g = (Gene) obj;
+                            n4jSymbols.add(g.getMarkerSymbol());
+                        }
+                        break;
+                    }
+
                 }
             }
         }
 
+        // get result from SOLR
+        SolrQuery query = new SolrQuery();
+        query.setQuery("*:*");
+        query.addFilterQuery("mp_term_name:\"" + mpTerm + "\" OR intermediate_mp_term_name:\"" + mpTerm + "\" OR top_level_mp_term_name:\"" + mpTerm + "\"");
+        query.setStart(0);
+        query.setRows(99999);
+        query.setParam("fl", "marker_symbol");
+
+        System.out.println("SOLR QUERY: " + query);
+        QueryResponse response = genotypePhenotypeCore.query(query);
+        //System.out.println("response: " + response);
+
+        Set<String> geneSymbolsGP = new HashSet<>();
+
+        //long genesFoundSolr = response.getResults().getNumFound();
+        for (SolrDocument doc : response.getResults()) {
+            geneSymbolsGP.add(doc.getFieldValue("marker_symbol").toString());
+        }
+
+        int genesFoundSolr = geneSymbolsGP.size();
+
+        System.out.println("\nCompare number of unique genes associated with phenotype '" + mpTerm + "'\nfrom Neo4j and SOLR genotype-phenotype core:\n");
+        String comp = genesFoundNeo4j == genesFoundSolr ? "SUCCESS: " : "FAILED: ";
+        comp += "Neo4j [" + genesFoundNeo4j + "] vs SOLR: [" + genesFoundSolr + "]";
+        System.out.println(comp + "\n\n");
+
+        Set<String> missingGeneSymbolN4j = new HashSet<>();
+
+        for (String gs : geneSymbolsGP){
+            if ( ! n4jSymbols.contains(gs)){
+                missingGeneSymbolN4j.add(gs);
+            }
+        }
+
+        System.out.println(missingGeneSymbolN4j.size() + " Neo4j gene symbols not in GP\n");
+        System.out.println(missingGeneSymbolN4j);
 
     }
+
+    @Test
+    public void testMarkerSymbolsInGPAndStatisticalResultCoreWithMp() throws IOException, SolrServerException {
+        //String mpTerm = "abnormal glucose homeostasis";
+        String mpTerm = "abnormal retina morphology";
+
+        // get result from SOLR
+        SolrQuery query = new SolrQuery();
+        query.setQuery("*:*");
+        query.addFilterQuery("mp_term_name:\"" + mpTerm + "\" OR intermediate_mp_term_name:\"" + mpTerm + "\" OR top_level_mp_term_name:\"" + mpTerm + "\"");
+        query.setStart(0);
+        query.setRows(99999);
+        query.setParam("fl", "marker_symbol");
+
+        System.out.println("SOLR QUERY: " + query);
+        QueryResponse response = genotypePhenotypeCore.query(query);
+        //System.out.println("response: " + response);
+
+        Set<String> geneSymbolsGP = new HashSet<>();
+
+        for (SolrDocument doc : response.getResults()){
+            geneSymbolsGP.add(doc.getFieldValue("marker_symbol").toString());
+        }
+
+        // ignores significant:true
+
+        SolrQuery query2 = new SolrQuery();
+        query2.setQuery("*:*");
+        //query2.addFilterQuery("significant:true AND (mp_term_name:\"" + mpTerm + "\" OR intermediate_mp_term_name:\"" + mpTerm + "\" OR top_level_mp_term_name:\"" + mpTerm + "\")");
+        query2.addFilterQuery("(mp_term_name:\"" + mpTerm + "\" OR intermediate_mp_term_name:\"" + mpTerm + "\" OR top_level_mp_term_name:\"" + mpTerm + "\")");
+        query2.setStart(0);
+        query2.setRows(99999);
+        query2.setParam("fl", "marker_symbol");
+
+        System.out.println("SOLR QUERY: " + query2);
+        QueryResponse response2 = statisticalResultCore.query(query2);
+        //System.out.println("response: " + response);
+
+        Set<String> geneSymbolsSR= new HashSet<>();
+
+        for (SolrDocument doc : response2.getResults()){
+            geneSymbolsSR.add(doc.getFieldValue("marker_symbol").toString());
+        }
+
+        System.out.println("Test consistency of MP to gene association of GP and SR cores:\n");
+        System.out.println("Unique gene symbols in GP core: " + geneSymbolsGP.size());
+        System.out.println("Unique gene symbols in SR core: " + geneSymbolsSR.size());
+
+        int missing = 0;
+        for (String gs : geneSymbolsGP){
+            if (! geneSymbolsSR.contains(gs) ){
+                System.out.println(gs + " not in SR core");
+                missing++;
+            }
+        }
+
+        System.out.println();
+        if (missing == 0){
+            System.out.println("SUCCESS: '" + mpTerm + "' associated with genes consistent in GP core and SR core ");
+        }
+        else {
+            System.out.println("FAILED: '" + mpTerm + "' associated with genes INconsistent in GP core and SR core ");
+        }
+
+
+    }
+
+    //@Test
+    public void testMarkerSymbolsInGPCoreMissingInAllele2Core() throws IOException, SolrServerException {
+
+        // get result from SOLR
+        SolrQuery query = new SolrQuery();
+        query.setQuery("*:*");
+        query.setStart(0);
+        query.setRows(99999);
+        query.setParam("fl", "marker_symbol");
+        query.setParam("fq", "type:Allele");
+
+        System.out.println("SOLR QUERY: " + query);
+        QueryResponse response = allele2Core.query(query);
+        //System.out.println("response: " + response);
+
+        Set<String> geneSymbolsAllele2 = new HashSet<>();
+
+        for (SolrDocument doc : response.getResults()){
+            geneSymbolsAllele2.add(doc.getFieldValue("marker_symbol").toString());
+        }
+
+        SolrQuery query2 = new SolrQuery();
+        query2.setQuery("marker_symbol:*");
+        query2.setStart(0);
+        query2.setRows(99999);
+        query2.setParam("fl", "marker_symbol, allele_symbol");
+
+        System.out.println("SOLR QUERY: " + query2);
+        QueryResponse response2 = genotypePhenotypeCore.query(query2);
+
+
+        Set<String> geneSymbolsGP = new HashSet<>();
+        Map<String, String> geneAllele = new HashMap<>();
+
+        for (SolrDocument doc : response2.getResults()){
+            String gs = doc.getFieldValue("marker_symbol").toString();
+            geneSymbolsGP.add(gs);
+
+            geneAllele.put(gs, doc.getFieldValue("allele_symbol").toString());
+        }
+
+        Set<String> missingGeneSymbolsAllele2 = new HashSet<>();
+        Set<String> missingGeneSymbolsGP = new HashSet<>();
+
+        // gene symbols in Allele2 BUT NOT in GP
+        for(String gs : geneSymbolsAllele2){
+            if (! geneSymbolsGP.contains(gs)){
+                missingGeneSymbolsAllele2.add(gs);
+            }
+        }
+
+        // gene symbols in GP BUT NOT in Allele2
+        for(String gs : geneSymbolsGP){
+            if (! geneSymbolsAllele2.contains(gs)){
+                missingGeneSymbolsGP.add(gs);
+            }
+        }
+
+
+        System.out.println(missingGeneSymbolsGP.size() + " GP core gene symbols NOT in Allele2 core:");
+        System.out.println(missingGeneSymbolsGP + "\n");
+        for (String gs : missingGeneSymbolsGP){
+            System.out.println(gs + " -- " + geneAllele.get(gs));
+        }
+
+        System.out.println("\n" + missingGeneSymbolsAllele2.size() + " Allele2 core gene symbols NOT in GP core: ");
+        System.out.println(missingGeneSymbolsAllele2);
+
+
+
+
+
+    }
+
 
 
 //	@Test
