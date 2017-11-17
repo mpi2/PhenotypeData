dbc86b03971387a90e6eed363944f6c3871324df
mrelac <mrelac@ebi.ac.uk>
2017-11-17 19:05:08 +0000

    Updated test.

--- loads/src/test/java/org/mousephenotype/cda/loads/integration/data/DataIntegrationTest.java
+++ loads/src/test/java/org/mousephenotype/cda/loads/integration/data/DataIntegrationTest.java
@@ -16,8 +16,10 @@
 
 package org.mousephenotype.cda.loads.integration.data;
 
+import org.junit.Assert;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.mousephenotype.cda.db.pojo.BiologicalSample;
 import org.mousephenotype.cda.loads.common.CdaSqlUtils;
 import org.mousephenotype.cda.loads.common.DccSqlUtils;
 import org.mousephenotype.cda.loads.create.extract.dcc.ExtractDccExperiments;
@@ -29,10 +31,15 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.core.io.Resource;
+import org.springframework.jdbc.core.namedparam.NamedParameterJdbcTemplate;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
 import javax.sql.DataSource;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 /**
  * This is an end-to-end integration data test class that uses an in-memory database to populate a small dcc, cda_base,
@@ -70,6 +77,9 @@ public class DataIntegrationTest {
 
     @Autowired
     private CdaSqlUtils cdaSqlUtils;
+
+    @Autowired
+    private NamedParameterJdbcTemplate jdbcCda;
 
 
 
@@ -131,6 +141,15 @@ public class DataIntegrationTest {
         System.out.println("loadExperiments");
         loadExperiments.run();
 
+        List<List<String>> results = getSpecimenStrainAccs();
+        List<String> controlList = results.get(0);
+        List<String> mutantList = results.get(1);
+
+//        Assert.assertEquals("abc", controlList.get(0), mutantList.get(0));
+        if ((controlList.get(0) != mutantList.get(0)) || (controlList.get(1) != mutantList.get(1))) {
+            Assert.fail("control and mutant strains differ:\n\tcontrol: " + controlList.get(0) + "::" + controlList.get(1) + "\n\t" + mutantList.get(0) + "::" + mutantList.get(1));
+        }
+
 
 
 
@@ -153,5 +172,57 @@ public class DataIntegrationTest {
 //                System.out.println("\t" + simpleParameter.getParameterID() + " :: " + simpleParameter.getValue());
 //            }
 //        }
+    }
+
+
+    // PRIVATE METHODS
+
+
+    /**
+    Returns:
+    <ul>
+        <li>control strain acc, control strain name</li>
+        <li>mutant strain acc, mutant strain name</li>
+     </ul>
+     */
+    private List<List<String>> getSpecimenStrainAccs() {
+
+        List<List<String>> results = new ArrayList<>();
+
+        String query = "SELECT bs.*, s.* FROM biological_sample bs " +
+                "LEFT OUTER JOIN biological_model_sample bms ON bms.biological_sample_id = bs.id " +
+                "LEFT OUTER JOIN biological_model bm ON bm.id = bms.biological_model_id " +
+                "LEFT OUTER JOIN biological_model_strain bmstrain ON bmstrain.biological_model_id = bm.id " +
+                "LEFT OUTER JOIN strain s ON s.acc = bmstrain.strain_acc " +
+                "WHERE bs.external_id IN ('19603', '14819')";
+
+        Map<String, Object> parameterMap = new HashMap<>();
+
+        List<Map<String, Object>> listMap     = jdbcCda.queryForList(query, parameterMap);
+        List<String>              controlList = new ArrayList<>();
+        List<String>              mutantList  = new ArrayList<>();
+        for (Map<String, Object> map : listMap) {
+            Object sampleGroup = map.get("sample_group");
+            Object acc         = map.get("acc");
+            Object name        = map.get("name");
+
+            if (sampleGroup != null) {
+                switch (sampleGroup.toString()) {
+                    case "control":
+                        controlList.add(acc == null ? "" : acc.toString());
+                        controlList.add(name == null ? "" : name.toString());
+                        break;
+
+                    default:
+                        mutantList.add(acc == null ? "" : acc.toString());
+                        mutantList.add(name == null ? "" : name.toString());
+                }
+            }
+
+            results.add(controlList);
+            results.add(mutantList);
+        }
+
+        return results;
     }
 }
--- loads/src/test/resources/sql/h2/dataIntegrationTest-data.sql
+++ loads/src/test/resources/sql/h2/dataIntegrationTest-data.sql
@@ -1,5 +1,6 @@
 -- database: cda_test
 
+-- For testBackgroundStrainIsEqual
 INSERT INTO ontology_term (acc, db_id, name, is_obsolete, replacement_acc) VALUES
   ('EFO:0002948', 15, 'postnatal',          0, null),
   ('EFO:0005857', 15, 'mouse embryo stage', 0, null),
