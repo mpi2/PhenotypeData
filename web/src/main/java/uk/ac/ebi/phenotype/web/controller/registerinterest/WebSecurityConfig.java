92df9f078ba3131590ec24a52e68b9901dfd9c77
jmason-ebi <jmason@ebi.ac.uk>
2018-11-01 16:46:40 +0000

    Add more allowed origins for X-Frames

--- web/src/main/java/uk/ac/ebi/phenotype/web/controller/registerinterest/WebSecurityConfig.java
+++ web/src/main/java/uk/ac/ebi/phenotype/web/controller/registerinterest/WebSecurityConfig.java
@@ -87,7 +87,7 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
     @Override
     protected void configure(HttpSecurity http) throws Exception {
 
-        AllowFromStrategy strategy = httpServletRequest -> "www.immunophenotype.org, wwwdev.ebi.ac.uk";
+        AllowFromStrategy strategy = httpServletRequest -> "www.immunophenotype.org, www.ebi.ac.uk, wwwdev.ebi.ac.uk, www.mousephenotype.org, beta.mousephenotype.org, dev.mousephenotype.org";
 
         http
             .addFilterAfter(captchaFilter, CsrfFilter.class)
