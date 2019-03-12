073e31b0f526dac6d571c7defc3cb15c7d6868a4
jmason <jmason@ebi.ac.uk>
2019-03-12 23:05:53 +0000

    Don't inherit static method

--- loads/src/main/java/org/mousephenotype/cda/loads/statistics/load/windowing/WindowingStatisticalResultLoader.java
+++ loads/src/main/java/org/mousephenotype/cda/loads/statistics/load/windowing/WindowingStatisticalResultLoader.java
@@ -12,6 +12,7 @@ import org.mousephenotype.cda.loads.statistics.load.StatisticalResultLoaderConfi
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Import;
 import org.springframework.context.annotation.Profile;
@@ -43,6 +44,9 @@ public class WindowingStatisticalResultLoader extends StatisticalResultLoader im
         this.mpTermService = mpTermService;
     }
 
+    public static void main(String[] args) {
+        SpringApplication.run(StatisticalResultLoader.class, args);
+    }
 
     @Override
     public void run(String... strings) throws Exception {
