7123db050670153a6012b5b14b9fea50a0451834
“Mike <mrelac@ebi.ac.uk>
2019-04-25 13:39:57 +0100

    Added FIXME comment in CdabaseConfig

--- loads/src/main/java/org/mousephenotype/cda/loads/create/extract/cdabase/config/CdabaseConfig.java
+++ loads/src/main/java/org/mousephenotype/cda/loads/create/extract/cdabase/config/CdabaseConfig.java
@@ -74,6 +74,7 @@ public class CdabaseConfig extends DataSourceCdabaseConfig {
 
     private Map<DownloadFileEnum, DownloadFilename> downloadFilenameMap = new HashMap<>();
 
+    // FIXME FIXME FIXME Replace this definition with a system-known location like /tmp
     @NotNull
     @Value("${cdabase.workspace}")
     protected String cdabaseWorkspace;
