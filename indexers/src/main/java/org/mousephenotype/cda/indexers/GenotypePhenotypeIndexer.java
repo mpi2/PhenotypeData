162053195a50a505b98f107dd09db4001f9bb5a1
jmason <jmason@ebi.ac.uk>
2019-04-28 09:09:28 +0100

    print documents

--- indexers/src/main/java/org/mousephenotype/cda/indexers/GenotypePhenotypeIndexer.java
+++ indexers/src/main/java/org/mousephenotype/cda/indexers/GenotypePhenotypeIndexer.java
@@ -372,6 +372,8 @@ final int MAX_MISSING_LIFE_STAGE_ERRORS_TO_LOG = 100;
 
                 documentCount++;
                 genotypePhenotypeCore.addBean(doc, 30000);
+                logger.error("Adding document: {}", doc);
+                genotypePhenotypeCore.commit();
 
                 count++;
             }
