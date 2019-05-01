33e392cbbffe753603d2cc0bfb4ccaf039cbd19c
jmason <jmason@ebi.ac.uk>
2019-05-01 10:02:09 +0100

    put back concurrent loading

--- indexers/src/main/java/org/mousephenotype/cda/indexers/GenotypePhenotypeIndexer.java
+++ indexers/src/main/java/org/mousephenotype/cda/indexers/GenotypePhenotypeIndexer.java
@@ -371,8 +371,7 @@ final int MAX_MISSING_LIFE_STAGE_ERRORS_TO_LOG = 100;
                 }
 
                 documentCount++;
-                genotypePhenotypeCore.addBean(doc);
-                genotypePhenotypeCore.commit();
+                genotypePhenotypeCore.addBean(doc, 30000);
 
                 count++;
             }
