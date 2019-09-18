149c18ec1972ab30caf4938e7b6651c45c663c93
jmason-ebi <jmason@ebi.ac.uk>
2019-09-18 13:37:22 +0100

    Provide an ImpressService bean to the configuration

--- loads/src/main/java/org/mousephenotype/cda/loads/create/extract/impressbase/ImpressbaseConfig.java
+++ loads/src/main/java/org/mousephenotype/cda/loads/create/extract/impressbase/ImpressbaseConfig.java
@@ -17,13 +17,18 @@
 package org.mousephenotype.cda.loads.create.extract.impressbase;
 
 import org.mousephenotype.cda.loads.common.config.DataSourceCdabaseConfig;
+import org.mousephenotype.cda.loads.create.extract.cdabase.support.ImpressUtils;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.data.jpa.JpaRepositoriesAutoConfiguration;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceTransactionManagerAutoConfiguration;
 import org.springframework.boot.autoconfigure.jms.JndiConnectionFactoryAutoConfiguration;
 import org.springframework.boot.autoconfigure.orm.jpa.HibernateJpaAutoConfiguration;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+
+import javax.validation.constraints.NotNull;
 
 /**
  * Created by mrelac on 03/05/16.
@@ -38,5 +43,13 @@ import org.springframework.context.annotation.Configuration;
 })
 public class ImpressbaseConfig extends DataSourceCdabaseConfig {
 
+    @NotNull
+    @Value("${impress.service.url}")
+    String impressServiceUrl;
+
+    @Bean
+    ImpressUtils impressUtils () {
+        return new ImpressUtils(impressServiceUrl);
+    }
 
 }
