3816b45ac8be30c381e4836ccd22a6d3831fc601
Kola Babalola <kola@ebi.ac.uk>
2019-10-02 16:42:08 +0100

    Working version with holding directory. Contains extra code to map urls to new destinations in Harwell

--- external_tools/src/main/python/images/downloadimages.py
+++ external_tools/src/main/python/images/downloadimages.py
@@ -17,13 +17,18 @@ totalNumberOfImagesWeHave=0
 numFoundInSolr=0
 
 
+### REMOVE ###
+fname = "/home/kola/code/embl_projects/PhenotypeData/external_tools/src/main/resources/harwell_old_url_to_new_url_map.txt"
+with open(fname,'rt') as fid:                                          
+    url_map = {l.split()[0]:l.split()[1] for l in fid.readlines()}
+
 uniqueUris=set()
 
 def main(argv):
-    """Download images referred to in solr index onto disk"""
+    """Download images referred to in solr index onto holding area on disk"""
     
     parser = argparse.ArgumentParser(
-        description='Download images referred to in solr index onto disk'
+        description='Download images referred to in solr index onto holding area on disk'
     )
     parser.add_argument('-d', '--rootDestinationDir', dest='rootDestinationDir',
                         help='Directory for root of destination to store images'    )
@@ -38,6 +43,9 @@ def main(argv):
     )
     parser.add_argument('-db', '--database', dest='komp2Db',
                         help='Database to connect to for komp2db'
+    )
+    parser.add_argument('--not-downloaded', dest='notDownloadedOutputPath',
+                        help='path to save list of files that could not be downloaded'
     )
     parser.add_argument('--pass', dest='komp2Pass',
                         help='Password for komp2db'
@@ -73,30 +81,45 @@ def main(argv):
 
     rootSolrUrl = args.rootSolrUrl if args.rootSolrUrl<>None else omeroProps['solrurl']
     #solrQuery="""experiment/select?q=observation_type:image_record&fq=download_file_path:(download_file_path:*bhjlk01.jax.org/images/IMPC_ALZ_001/*%20AND%20!download_file_path:*.mov)&fl=id,download_file_path,phenotyping_center,pipeline_stable_id,procedure_stable_id,datasource_name,parameter_stable_id&wt=json&indent=on&rows=10000000"""
-    solrQuery="""experiment/select?q=observation_type:image_record&fq=(download_file_path:*mousephenotype.org*%20AND%20!download_file_path:*.mov%20AND%20!download_file_path:*.bz2)&fl=id,download_file_path,phenotyping_center,pipeline_stable_id,procedure_stable_id,datasource_name,parameter_stable_id&wt=json&indent=on&rows=10000000"""
+    #solrQuery="""experiment/select?q=observation_type:image_record&fq=(download_file_path:*mousephenotype.org*%20AND%20!download_file_path:*.mov%20AND%20!download_file_path:*.bz2)&fl=id,download_file_path,phenotyping_center,pipeline_stable_id,procedure_stable_id,datasource_name,parameter_stable_id&wt=json&indent=on&rows=10000000"""
+    solrQuery="""experiment/select?q=observation_type:image_record&fq=(download_file_path:*mousephenotype.org*%20AND%20!download_file_path:*.mov%20AND%20!download_file_path:*.bz2)&fl=id,download_file_path,phenotyping_center,pipeline_stable_id,procedure_stable_id,datasource_name,parameter_stable_id&wt=json&indent=on&rows=10"""
 
     #note cant split this url over a few lines as puts in newlines into url which doesn't work
     rootDestinationDir = args.rootDestinationDir if args.rootDestinationDir<>None else omeroProps['rootdestinationdir'] 
-    print("running python image download script for impc images")
+    print "running python image download script for impc images"
 
     print 'rootDestinationDir is "', rootDestinationDir
     solrUrl=rootSolrUrl+solrQuery;
     print 'solrUrl', solrUrl
 
-    runWithSolrAsDataSource(solrUrl, rootDestinationDir)
+    notDownloaded = runWithSolrAsDataSource(solrUrl, rootDestinationDir)
+    notDownloadedOutputPath =  args.notDownloadedOutputPath if args.notDownloadedOutputPath <> None else createNotDownloadedOutputPath(rootDestinationDir)
+    
+    with open(notDownloadedOutputPath, 'wt') as fid:
+        fid.writelines(notDownloaded)
 
+    print str(len(notDownloaded)) + " files could not be downloaded. Written to " + notDownloadedOutputPath
 
 def runWithSolrAsDataSource(solrUrl, rootDestinationDir):
     """
-    need to get these passed in as arguments - the host and db name etc for jenkins to run
-    first get the list of download urls and the data source, experiment, procdure and parameter and observation id for the images
+        Download images using Solr as the datasource.
+        Return urls that cannot be downloaded
     """
+
+    ### REMOVE ###
+    global url_map
+
+    notDownloaded = []
     v = json.loads(requests.get(solrUrl).text)
     docs=v['response']['docs']
     numFoundInSolr=v['response']['numFound']
     for doc in docs:
         download_file_path=doc['download_file_path']
         download_file_path=download_file_path.lower()
+        ### REMOVE ###
+        to_replace = os.path.split(download_file_path)[0] + "/"
+        to_replace = to_replace.replace('http://images.mousephenotype.org','')
+        download_file_path =  download_file_path.replace(to_replace, url_map[to_replace])
         datasource_id=doc['datasource_name']
         phenotyping_center=doc['phenotyping_center']
         #experiment=doc['experiment']
@@ -104,13 +127,28 @@ def runWithSolrAsDataSource(solrUrl, rootDestinationDir):
         observation_id=doc['id']
         procedure_stable_id=doc['procedure_stable_id']
         parameter_stable_id=doc['parameter_stable_id']
-        processFile(observation_id, rootDestinationDir,phenotyping_center,pipeline_stable_id, procedure_stable_id, parameter_stable_id, download_file_path)
+        downloaded = processFile(observation_id, rootDestinationDir,phenotyping_center,pipeline_stable_id, procedure_stable_id, parameter_stable_id, download_file_path)
+        if not downloaded:
+            notDownloaded.append(download_file_path+'\n')
         
     print 'number found in solr='+str(numFoundInSolr)+' number of failed responses='+str(responseFailed)+' number of requests='+str(numberOfImageDownloadAttemps)+' total totalNumberOfImagesWeHave='+str(totalNumberOfImagesWeHave)
+    return notDownloaded
 
 def createDestinationFilePath(rootDestinationDir, phenotyping_center, pipeline_stable_id, procedure, parameter, download_file_path):
     directory="/".join([rootDestinationDir,phenotyping_center, pipeline_stable_id,procedure,parameter])
     return directory
+
+def createNotDownloadedOutputPath(rootDestinationDir):
+    """
+        Create path to store files that could not be downloaded.
+        This uses the current date and time and the root destination dir
+    """
+    
+    import time
+    from datetime import datetime
+
+    today = datetime.fromtimestamp(time.time())
+    return os.path.join(rootDestinationDir, today.strftime("%Y%m%d_%H%M%S_could_not_download.txt"))
 
 def processFile(observation_id,  rootDestinationDir, phenotyping_center,pipeline_stable_id, procedure, parameter, downloadFilePath):
     global totalNumberOfImagesWeHave
@@ -124,7 +162,7 @@ def processFile(observation_id,  rootDestinationDir, phenotyping_center,pipeline
     fullResolutionFilePath=dstfilename.split(splitString,1)[1]
     #print 'saving file to '+dstfilename
     if os.path.isfile(dstfilename):
-        #print("file already here")
+        #print "file already here"
         totalNumberOfImagesWeHave+=1
     else:
         numberOfImageDownloadAttemps+=1
@@ -134,6 +172,7 @@ def processFile(observation_id,  rootDestinationDir, phenotyping_center,pipeline
         if response.status_code != 200:
             print "Error status code is not 200="+str(response.status_code)+"downloadFilePath:"+downloadFilePath
             responseFailed+=1
+            return False
         if response.status_code == 200:
             totalNumberOfImagesWeHave+=1
             #check directory exists before trying to write file and if not then make it
@@ -147,7 +186,7 @@ def processFile(observation_id,  rootDestinationDir, phenotyping_center,pipeline
     if totalNumberOfImagesWeHave%10000==0 :
         print "totalNumber of images we have="+str(totalNumberOfImagesWeHave)
 
-
+    return True
 
 if __name__ == "__main__":
     main(sys.argv[1:])
