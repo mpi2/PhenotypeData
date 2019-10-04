36864429fafa39b1e71bedfa6aba09b6ef5daf2f
jmason-ebi <jmason@ebi.ac.uk>
2019-10-04 09:26:50 +0100

    Disable wbe env for Generating derived parameters

--- loads/src/main/java/org/mousephenotype/cda/loads/derived/GenerateDerivedParameters.java
+++ loads/src/main/java/org/mousephenotype/cda/loads/derived/GenerateDerivedParameters.java
@@ -16,8 +16,9 @@ import org.mousephenotype.cda.loads.common.ConcurrentHashMapAllowNull;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.boot.CommandLineRunner;
-import org.springframework.boot.SpringApplication;
+import org.springframework.boot.WebApplicationType;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.context.annotation.Import;
 
 import javax.inject.Inject;
@@ -3283,6 +3284,8 @@ public class GenerateDerivedParameters implements CommandLineRunner {
 
 
     public static void main(String[] args) {
-        SpringApplication.run(GenerateDerivedParameters.class, args);
+        new SpringApplicationBuilder(GenerateDerivedParameters.class)
+                .web(WebApplicationType.NONE)
+                .run(args);
     }
 }
