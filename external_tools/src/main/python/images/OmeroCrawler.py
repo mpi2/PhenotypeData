661b48182d7dac6026bc3e609961400c40067b7c
jwgwarren <1829853+jwgwarren@users.noreply.github.com>
2019-10-30 15:11:54 +0000

    Update OmeroCrawler.py
    temporarily replace live link from solr with omero dev link

--- external_tools/src/main/python/images/OmeroCrawler.py
+++ external_tools/src/main/python/images/OmeroCrawler.py
@@ -65,6 +65,8 @@ class OmeroCrawler:
                 if context is not None:
                     #http://www.ebi.ac.uk/mi/media/omero/webgateway/render_thumbnail/141436
                     #http://ves-oy-ca/omero/webgateway/render_thumbnail/89930/200/
+                    #temporarily replace live link from solr with omero dev link 
+                     url=url.replace('www.ebi.ac.uk/mi/media','wwwdev.ebi.ac.uk/mi/media)
                     if context == 'live':
                         #print "context is live and omero host=" + self.omeroHost 
                         url=url.replace('www.ebi.ac.uk/mi/media',self.omeroHost)
