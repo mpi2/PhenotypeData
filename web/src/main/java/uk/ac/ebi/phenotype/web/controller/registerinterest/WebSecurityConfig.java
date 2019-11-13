f763ee755d94663d5f50b79c9070fd1c7b97bc74
jmason-ebi <jmason@ebi.ac.uk>
2019-11-13 01:49:44 +0000

    Use response redirect (instead of strategy)

--- web/src/main/java/uk/ac/ebi/phenotype/web/controller/registerinterest/WebSecurityConfig.java
+++ web/src/main/java/uk/ac/ebi/phenotype/web/controller/registerinterest/WebSecurityConfig.java
@@ -16,8 +16,6 @@
 
 package uk.ac.ebi.phenotype.web.controller.registerinterest;
 
-import org.apache.commons.logging.Log;
-import org.apache.commons.logging.LogFactory;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -213,23 +211,13 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
      */
     public class RiSavedRequestAwareAuthenticationSuccessHandler implements AuthenticationSuccessHandler {
 
-        protected Log logger = LogFactory.getLog(this.getClass());
-        private RedirectStrategy redirectStrategy = new DefaultRedirectStrategy();
+        private final Logger logger = LoggerFactory.getLogger(this.getClass().getCanonicalName());
 
         @Override
         public void onAuthenticationSuccess(HttpServletRequest request,
                                             HttpServletResponse response,
                                             Authentication authentication)
                 throws IOException {
-            handle(request, response, authentication);
-            clearAuthenticationAttributes(request);
-        }
-
-
-        protected void handle(HttpServletRequest request,
-                              HttpServletResponse response, Authentication authentication)
-                throws IOException {
-
             String targetUrl = getBaseUrl(request) + "/summary";
 
             if (response.isCommitted()) {
@@ -237,8 +225,10 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
                 return;
             }
 
-            redirectStrategy.sendRedirect(request, response, targetUrl);
+            response.sendRedirect(targetUrl);
+            clearAuthenticationAttributes(request);
         }
+
 
         protected void clearAuthenticationAttributes(HttpServletRequest request) {
             HttpSession session = request.getSession(false);
