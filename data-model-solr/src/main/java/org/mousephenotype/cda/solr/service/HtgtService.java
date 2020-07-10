1ace532bc51f89d043d2454c417655457966f41e
jwarren <warren.jonathan@gmail.com>
2020-07-10 11:26:11 +0100

    High throughput gene targeting page lists oligo designs by design_id

--- data-model-solr/src/main/java/org/mousephenotype/cda/solr/service/HtgtService.java
+++ data-model-solr/src/main/java/org/mousephenotype/cda/solr/service/HtgtService.java
@@ -3,6 +3,7 @@ package org.mousephenotype.cda.solr.service;
 import org.springframework.stereotype.Service;
 
 import javax.inject.Inject;
+import java.util.List;
 
 /**
  * Service to get High Throughput Gene Targeting Designs
@@ -16,7 +17,9 @@ public class HtgtService {
     public HtgtService(HtgtDataAccess htgtDataAccess){
         this.htgtDataAccess=htgtDataAccess;
     }
-    public String getDesigns(String designId){
-        return htgtDataAccess.getDesigns(designId);
+    public List<Design> getDesigns(int designId){
+        DesignsResponse designsResponse = htgtDataAccess.getDesigns(designId);
+        return designsResponse.getData().getOligos();
+
     }
 }
