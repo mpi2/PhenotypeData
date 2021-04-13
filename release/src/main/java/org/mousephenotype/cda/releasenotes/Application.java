c675ae34eeb0a1855cbcd87daab6e26b7feba31f
jmason <jmason@ebi.ac.uk>
2021-04-13 15:11:26 +0100

    Add cache manager for release manager

--- release/src/main/java/org/mousephenotype/cda/releasenotes/Application.java
+++ release/src/main/java/org/mousephenotype/cda/releasenotes/Application.java
@@ -5,12 +5,14 @@ import org.springframework.boot.Banner;
 import org.springframework.boot.WebApplicationType;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.context.ConfigurableApplicationContext;
 
 /**
  * Spring boot application that will run the ReleaseAnalyticsManager
  */
 @SpringBootApplication
+@EnableCaching
 public class Application {
 
     public static void main(String args[]) {
