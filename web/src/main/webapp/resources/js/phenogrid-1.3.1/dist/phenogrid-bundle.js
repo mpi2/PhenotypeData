84f3fd1123747adc842003bbde750b9eca00fede
dpavam <hhz161@qmul.ac.uk>
2024-01-30 11:47:17 +0000

    testing CI change delay

--- web/src/main/webapp/resources/js/phenogrid-1.3.1/dist/phenogrid-bundle.js
+++ web/src/main/webapp/resources/js/phenogrid-1.3.1/dist/phenogrid-bundle.js
@@ -504,6 +504,7 @@ DataLoader.prototype = {
             
             jqxhr.fail(function () { 
                 console.log('Ajax error - processDataForVendor()')
+                console.log('test message to check CI delay');
             });
 		} else {
 			this.postDataLoadCallback();  // make a call back to post data init function
