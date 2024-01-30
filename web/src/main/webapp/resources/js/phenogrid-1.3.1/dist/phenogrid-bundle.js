49a171f8a406734e2b5f1884a708ff97312c50b5
dpavam <hhz161@qmul.ac.uk>
2024-01-30 11:44:24 +0000

    Revert "test print statements"
    This reverts commit ef5f69d5159e399e87ceff3340bb72096edabb37.

--- web/src/main/webapp/resources/js/phenogrid-1.3.1/dist/phenogrid-bundle.js
+++ web/src/main/webapp/resources/js/phenogrid-1.3.1/dist/phenogrid-bundle.js
@@ -394,8 +394,6 @@ DataLoader.prototype = {
 			asyncDataLoadingCallback - callback
 	*/
     loadCompareData: function(targetGroup, qrySourceList, geneList, asyncDataLoadingCallback) {
-      // Print for debugging purposes
-    console.log('loadCompareData()');
 		this.postDataLoadCallback = asyncDataLoadingCallback;
         
         // save the original source listing
@@ -438,8 +436,6 @@ DataLoader.prototype = {
     
     // In progress 03/09/2016
 	loadCompareDataForVendor: function(qrySourceList, targetGroupList, asyncDataLoadingCallback, multiTargetsModeTargetLengthLimit) {
-    // Print for debugging purposes
-    console.log('loadCompareDataForVendor()');
 		// save the original source listing
         // The qrySourceList has already had all duplicated IDs removed in _parseQuerySourceList() of phenogrid.js - Joe
 		this.origSourceList = qrySourceList;
@@ -2216,8 +2212,6 @@ var images = require('./images.json');
         
         // Monarch analyze/phenotype compare use case
         _initCompare: function() {
-          // Print for debugging purposes
-            console.log('__initCompare()');
             // overwrite the this.state.targetGroupList with only 'compare'
             // this 'compare' is used in dataLoader.loadCompareData() and dataManager.buildMatrix() too - Joe
             var compare = "compare";
@@ -2265,8 +2259,6 @@ var images = require('./images.json');
 
         // Vendor (E.g., IMPC) use case or similar
         _initGridSkeletonDataForVendor: function() {
-            // Print for debugging purpose
-            console.log('Grid Sekeleton Data For Vendor');
             // Compose the target group list based on gridSkeletonData.xAxis
             for (var j = 0; j < this.state.gridSkeletonData.xAxis.length; j++) {
                 // E.g., {groupName: "Homo sapiens", groupId: "9606"}
